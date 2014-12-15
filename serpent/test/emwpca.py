825d4f9fe73bee08c09f3da67f106a7e2867f065
Jack Peterson <jack@tinybike.net>
2014-12-14 23:53:24 -0800

    raw and final event outcomes

--- serpent/test/emwpca.py
+++ serpent/test/emwpca.py
@@ -111,6 +111,14 @@ def get_weight(v):
         v += 1
     return v / sum_v
 
+def catch(X):
+    if X < 0.5 * (1 - 0.1):
+        return 0
+    elif X > 0.5 * (1 + 0.1):
+        return 1
+    else:
+        return .5
+
 def test_emwpca():
     def nparray2fixedlist(a):
         return map(lambda x: int(x), (a * 0x10000000000000000).tolist())
@@ -158,25 +166,21 @@ def test_emwpca():
 
     set1 = scores + abs(min(scores))
     set2 = scores - max(scores)
-    # print
     # print "set1:", set1
     # print "set2:", set2
 
     old = np.dot(weights, data)
-    # print
     # print "old: ", old
 
     wset1 = get_weight(set1)
     wset2 = get_weight(set2)
     # wset1 = map(hex, map(long, wset1 * 0x10000000000000000))
     # wset2 = map(hex, map(long, wset2 * 0x10000000000000000))
-    # print
     # print "wset1:", wset1
     # print "wset2:", wset2
 
     new1 = np.dot(wset1, data)
     new2 = np.dot(wset2, data)
-    # print
     # print "new1:", new1
     # print "new2:", new2
 
@@ -185,16 +189,22 @@ def test_emwpca():
         adj_prin_comp = set1
     if ref_ind > 0:
         adj_prin_comp = set2
-    print "adj_prin_comp:", adj_prin_comp
+    # print "adj_prin_comp:", adj_prin_comp
 
     row_reward_weighted = get_weight(weights)
     if max(abs(adj_prin_comp)) != 0:
         row_reward_weighted = get_weight(adj_prin_comp * (weights / np.mean(weights)).T)
-    print "row_reward_weighted:", row_reward_weighted
+    # print "row_reward_weighted:", row_reward_weighted
 
     alpha = 0.2
     smooth_rep = alpha*row_reward_weighted + (1-alpha)*get_weight(weights).T
-    print "smooth_rep:", smooth_rep
+    # print "smooth_rep:", smooth_rep
+
+    event_outcomes_raw = np.dot(smooth_rep, data).squeeze() / 10
+    # print "event_outcomes_raw:", event_outcomes_raw
+
+    event_outcomes_final = np.array(map(catch, event_outcomes_raw))
+    # print "event_outcomes_final:", event_outcomes_final
 
 if __name__ == '__main__':
     test_emwpca()
