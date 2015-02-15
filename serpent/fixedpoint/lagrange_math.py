0894ba98b43df6e99ad4981fff1be296fb37b714
Chris Calderon <chris-da-dev@augur.net>
2015-02-15 12:20:09 -0800

    Writing tools for testing

--- serpent/fixedpoint/lagrange_math.py
+++ serpent/fixedpoint/lagrange_math.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python2
-import gmpy2
-from pyethereum import tester as t
-import random
+from test_suite import *
+from collections import OrderedDict
 import numpy as np
 import scipy
 import scipy.interpolate
-from collections import namedtuple
-import sys
-
-gmpy2.get_context().precision = 256
-TestResults = namedtuple('TestResults', 'average_error average_gas')
 
 LN2 = hex(int(gmpy2.log(2.0)*2**64)).strip('L')
 LOG2E = hex(int(gmpy2.log2(gmpy2.exp(1))*2**64)).strip('L')
@@ -78,50 +72,24 @@ macro lpow2_{0}($x):
 def exp_test_code(max_n):
     return ''.join(map(exp_interp, range(1, max_n+1)))
 
-def test_thunk(thunk, trials):
-    error = 0
-    gas = 0
-    for i in range(int(trials)):
-        sys.stdout.write('Trials: %d\r'%i)
-        sys.stdout.flush()
-        expected, result = thunk()
-        result['output'] /= float(2**64)
-        error += abs(result['output'] - expected)/expected
-        gas += result['gas']
-    sys.stdout.write('\n')
-    sys.stdout.flush()
-    return TestResults(float(100*error/trials), gas/float(trials))
-
 def main():
     max_n = 14
+    ecode = exp_test_code(max_n)
+    lcode = log_test_code(max_n)
     trials = 100
-    if len(sys.argv) < 2:
-        print 'Usage:'
-        print '\t./lagrange_math.py <func>'
-        print 'Where func can be exp or log.'
-        sys.exit(1)
-    elif sys.argv[1] == 'exp':
-        code = exp_test_code(max_n)
-        ref_func = gmpy2.exp
-    elif sys.argv[1] == 'log':
-        code = log_test_code(max_n)
-        ref_func = gmpy2.log
-    else:
-        print "not implemented"
-        sys.exit(1)
-    print code
-    print 'Compiling...'
-    s = t.state()
-    c = s.abi_contract(code)
-    for i in range(1, max_n+1):
-        func = '%s%d' % (sys.argv[1], i)
-        print 'Testing', func
-        def thunk():
-            x = random.random()*128
-            testf = lambda x: vars(c)[func](int(x*2**64), profiling=True)
-            return ref_func(x), testf(x)
-        print test_thunk(thunk, trials)
-    sys.exit(0)
+
+    efunc_dict = OrderedDict()
+    exp_max = 128
+    for i in range(1,max_n+1):
+        efunc_dict['exp%d'%i] = ([0, exp_max], gmpy2.exp)
+
+    lfunc_dict = OrderedDict()
+    log_max = gmpy2.exp(128)
+    for i in range(1,max_n+1):
+        lfunc_dict['log%d'%i] = ([0, log_max], gmpy2.log)
+
+    test_code(ecode, efunc_dict, trials)
+    test_code(lcode, lfunc_dict, trials)
 
 if __name__ == '__main__':
     main()
---
+++ serpent/fixedpoint/test_suite.py
@@ -0,0 +1,71 @@
+import cStringIO
+import sys
+
+_ = sys.stderr
+sys.stderr = cStringIO.StringIO()
+from pyethereum import tester as t
+sys.stderr = _
+
+from collections import namedtuple
+import random
+import gmpy2
+import time
+
+gmpy2.get_context().precision = 256
+
+_TestResultBase = namedtuple('_TestResultBase', 'avg_error avg_gas')
+
+class TestResults(_TestResultBase):
+    def __str__(self):
+      return 'TestResults(avg_error={:%}%, avg_gas={})'.format(self[0], self[1])  
+
+def suppress_output(thunk):
+    old_stdout = sys.stdout
+    sys.stdout = cStringIO.StringIO()
+    try:
+        result = thunk()
+    except:
+        old_stdout.write(sys.stdout.read())
+        sys.exit(1)
+    else:
+        sys.stdout = old_stdout
+        return result
+
+def printf(fmt, *args):
+    sys.stdout.write(fmt % args)
+    sys.stdout.flush()
+
+def test_thunk(thunk, trials):
+    printf('Testing ' + thunk.__name__ + ':\n')
+    error = 0
+    gas = 0
+    for i in range(int(trials)):
+        printf('\tTrials: %d\r', i)
+        expected, result = thunk()
+        result['output'] /= float(2**64)
+        error += abs(result['output'] - expected)/expected
+        gas += result['gas']
+    printf('\n')
+    return TestResults(float(100*error/trials), gas/float(trials))
+
+def thunks(contract, func_dict):
+    for name, (bounds, ref_func) in func_dict.items():
+        def thunk(name=name, bounds=bounds, ref_func=ref_func):
+            test_func = vars(contract)[name]
+            x = random.random()*(bounds[1] - bounds[0]) + bounds[0]
+            return ref_func(x), test_func(int(x*2**64), profiling=True)
+        thunk.__name__ = name
+        yield thunk
+
+def compile(filename):
+    s = t.state()
+    start = time.time()
+    printf('Compiling...\t')
+    c = suppress_output(lambda: s.abi_contract(filename))
+    printf('Done in %.1f seconds.\n', time.time() - start)
+    return c
+
+def test_code(filename, func_dict, trials):
+    contract = compile(filename)
+    for thunk in thunks(contract, func_dict):
+        printf('\t' + str(test_thunk(thunk, trials)) + '\n')
