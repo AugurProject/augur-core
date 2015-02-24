49c4e154bb7300c17f5d5c35ebb2feba499f8f7c
Chris Calderon <chris-da-dev@augur.net>
2015-02-23 17:29:22 -0800

    Optimized optimizer

--- serpent/fixedpoint_tests/optimize_log.py
+++ serpent/fixedpoint_tests/optimize_log.py
@@ -7,6 +7,7 @@ import scipy.interpolate
 import numpy
 import random
 import itertools
+import time
 
 gmpy2.get_context().precision = 256
 
@@ -42,14 +43,18 @@ def avg_error(coeffs, trials, max):
     gmpy2.get_context().precision = 256
     total_error = 0
     for i in range(trials):
-        x = random.randrange(max)
+        x = random.random()*max
         expected = gmpy2.log(x) * 2**64
-        result = fxp_ln(x << 64, coeffs)
+        result = fxp_ln(int(x*2**64), coeffs)
         total_error += abs(result - expected)/expected
     return total_error/trials
  
-def worker(work, results, updates, trials, max):
-    coeffs = work.get()
+def worker(xs, results, updates, trials, max):
+    gmpy2.get_context().precision = 256
+    ys = (map(gmpy2.log2, x) for x in xs)
+    polys = (scipy.interpolate.lagrange(x, y) for x, y in itertools.izip(xs, ys))
+    coeffs = [[int(a_i*2**64) for a_i in reversed(p.coeffs)] for p in polys]
+    updates.put("Generated %d polynomials to test." % len(coeffs))
     min_err = float('inf')
     min_cs = None
     for cs in coeffs:
@@ -62,7 +67,7 @@ def worker(work, results, updates, trials, max):
             updates.put(str(exc))
             results.put((float('inf'), None))
             return
-    updates.put("Checked %d polynomials" % len(coeffs))
+    updates.put("Checked %d polynomials." % len(coeffs))
     results.put((min_err, min_cs))
 
 def pretty_errors(error_pair):
@@ -70,45 +75,38 @@ def pretty_errors(error_pair):
     print "%.6f%% error in %d terms." % (100*error, len(coeffs))
     print 'Coefficients:'
     print '\t' + '\n\t'.join(hex(a_i).strip("L") for a_i in coeffs)
-    print
 
 def rprint(string):
     sys.stdout.write('\r' + string)
     sys.stdout.flush()
 
 def main():
-    work = multiprocessing.Queue()
+    start = time.time()
     results = multiprocessing.Queue()
     updates = multiprocessing.Queue()
-    trials = 50
+    trials = 200
     max_input = int(gmpy2.exp(128))
     degree = int(sys.argv[2])
     num_points = int(sys.argv[1])
-    print "Generating interpolation polynomials of degree %d" % degree
-    xs = list(itertools.combinations(numpy.linspace(1, 2, num_points), degree))
-    ys = [map(gmpy2.log2, x) for x in xs]
-    polys = [scipy.interpolate.lagrange(x, y) for x, y in zip(xs, ys)]
-    coeffs = [[int(a_i*2**64) for a_i in reversed(p.coeffs)] for p in polys]
     n = multiprocessing.cpu_count()
     processes = []
     proc_results = []
-    print "Generated %d polynomials to check" % len(coeffs)
+    size = int(gmpy2.comb(num_points, degree)) / n
+    combs = itertools.combinations(numpy.linspace(1, 2, num_points), degree)
     for i in range(n):
-        print "putting work"
-        work.put(coeffs[i::n])
-        proc = multiprocessing.Process(target=worker, args=(work, results, updates, trials, max_input))
+        xs = [x for _, x in zip(range(size), combs)]
+        proc = multiprocessing.Process(target=worker, args=(xs, results, updates, trials, max_input))
         print "starting process %d" % i
         proc.start()
         processes.append(proc)
-    for i in range(n):
+    for i in range(2*n):
         print updates.get()
     for i in range(n):
         proc_results.append(results.get())
-        print "Got results!"
     for proc in processes:
         proc.join()
-        print "joined a proc"
     pretty_errors(min(proc_results))
-
+    print "Time Elapsed: %.2f seconds." % (time.time() - start)
+    
 if __name__ == '__main__':
     main()
