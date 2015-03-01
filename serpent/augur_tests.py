a95208805126cbe193afc2353fae476f02a8078c
Chris Calderon <chris-da-dev@augur.net>
2015-03-01 12:35:44 -0800

    testing updates

--- serpent/augur_tests.py
+++ serpent/augur_tests.py
@@ -2,6 +2,9 @@
 import time
 import random
 from pyethereum import tester as t
+import gmpy2
+gmpy2.get_context().precision = 256
+
 FILE = "augurLite.se"
 
 print "Compiling %s ... " % FILE
@@ -26,26 +29,47 @@ for i, (addr, key) in reversed(list(enumerate(a_k))):
     print "Sending %d to %s from %s" % (amt >> 64, to, addr)
     print ">>", augur.send(to, amt, sender=key)
 
-
-print "making a subbranch!"
+print "making a subbranch:", 
 subbranch = augur.makeSubBranch('"test branch"', 100, 1010101)
-print '>>', subbranch
-print "creating an event!"
-event = augur.createEvent(subbranch, '"will jack grow at least six inches in 2014?"', 1000, 0,1, 2)
-print '>>', event
-print "creating a market!"
-market = augur.createMarket(subbranch, '"Market on event %d"' % event, 1 << 60, 10 << 64, (1 << 64) + (1 << 60), [event])
-print '>>', market
+print subbranch
+
+print "creating some events:",
+event1 = augur.createEvent(
+    subbranch,                                           #branch 
+    '"Will Jack grow at least san inch in March 2015?"', #description
+    31*24*60*5,                                          #expiration block
+    0,                                                   #min value (binary)
+    1,                                                   #max value
+    2)                                                   #number of outcomes
+event2 = augur.createEvent(
+    subbranch, 
+    '"What will Jack\'s height be, from 60 to 72 inches, at noon PST on April 1st, 2015?"', 
+    31*24*60*5, 
+    60,                                                   #scalar event!
+    72, 
+    2) 
+print event1, event2
+
+print "creating a market:",
+market = augur.createMarket(
+    subbranch,
+    '"Market on events %d & %d"' % (event1, event2), 
+    1 << 60, 
+    10 << 64, 
+    (1 << 64) + (1 << 60), 
+    [event1, event2])
+print market
+
 bought = {}
 for addr, key in a_k:
-    x = random.randrange(1, 3)
-    print "%s is  buying a share of %d" % (addr, x)
-    myid = augur.buyShares(subbranch, market, x, 1, sender=key)
-    print '>>', myid
-    bought[addr] = (x, myid)
+    x = random.randrange(1, 5)
+    print "%s is  buying a share of %d:" % (addr, x),
+    cost = augur.buyShares(subbranch, market, x, 1, sender=key)
+    print "costs %d cashcoins" % cost
+    bought[addr] = x
 
 for addr, key in a_k:
-    share, myid = bought[addr]
-    paid = augur.sellShares(subbranch, market, share, 1, myid, sender=key)
-    print "%s got paid %f for selling 1 share of outcome %d" % (addr, paid/float(2**64), share)
+    share = bought[addr]
+    paid = augur.sellShares(subbranch, market, share, 1, sender=key)
+    print "%s got paid %f for selling 1 share of outcome %d" % (addr, paid/gmpy2.mpfr(1 << 64), share)
     
