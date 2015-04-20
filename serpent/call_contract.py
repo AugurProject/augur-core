440fdef8d1767a25a9af1962453a33f750c7a9c1
Joey Krug <joeykrug@gmail.com>
2015-04-19 18:22:55 -0700

    updates

--- serpent/call_contract.py
+++ serpent/call_contract.py
@@ -8,24 +8,16 @@ import sys
 import re
 
 contracts = {
-    'cash' : '0x70e1ca88480a4b5f719a0add9b557779b9691764',
-    'info' : '0x7f8bab7072ba2acc779dbb96d625bc73f6e6ccaf',
-    'branches' : '0x148a56efa88437f52b9b22020bf0e9fdc75350e1',
-    'events' : '0x45b5293570e78207f1c6dc41ca3e171c76b3d6db',
-    'expiringEvents' : '0xf20cfeb17a3266f640077c7426b0d4c5081ecf43',
+    'cash' : '0xc00329685aceba16e4bb622c9afc38aee6aa19eb',
+    'info' : '0xdff1e97c3d2885a843a985bcd9e0f078c20b24b9',
+    'branches' : '0xc00329685aceba16e4bb622c9afc38aee6aa19eb',
+    'events' : '0x947b07180469b17c6c299bc70c16fcd9ee9d5acc',
+    'expiringEvents' : '0x2491dee375afde12d3b443d0b3f9d686a1382158',
     'fxpFunctions' : '0x82695a58b84bbcc90372d06e9b1d599ca2dd60cd',
-    'markets' : '0xaee8391634b73b8cf9ea27ca1c0ca251a3ad184b',
-    'reporting' : '0xaab1a136fc3f3fce92fa1f8fe740e5e45ce6cde6',
-    'checkQuorum' : '0x7d687123131b7a931521adfc79cddff084e855f4',
-    'buy&sellShares' : '0x2094d578e8d7f20947f401328eec2eab7b07de06',
-    'createBranch' : '0xaad25f1fa3429929e682be94ef823257c877bbc3',
-    'p2pWagers' : '0x85e782b1ce91a1c0d8fdf93b0e9dfe52dc6ee5ef',
-    'sendReputation' : '0x8d4bb333a4b859c776994967ed6223b01dcd3a74',
-    'transferShares' : '0x63ee2b82fe40e1e103d130b4c2209d68cf047c93',
-    'makeReports' : '0xf45288dc3235e71e2504fd760ff745a4085584ad',
-    'createEvent' : '0xf3b7172641e46b28737cad3f07285d9f759054b9',
-    'createMarket' : '0x4f188693e4e67fa11ee25f83da0782f859daf02a',
-    'closeMarket' : '0xfd7baeaae87c0a9833d1d9840d8f4be554f4a9fa',
+    'markets' : '0xdff1e97c3d2885a843a985bcd9e0f078c20b24b9',
+    'reporting' : '0x2491dee375afde12d3b443d0b3f9d686a1382158',
+    'createEvent' : '0x3c17de81765a65e39923084732697971dfa42071',
+    'createMarket' : '0x143ee2baa908ee3641e07c0bed464e7e2b1ad5a4',
 }
 
 def get_sym(arg):
