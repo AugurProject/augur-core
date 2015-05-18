19859e63d999ca1e61d8719d7a3dbc40560688ad
Joey Krug <joeykrug@gmail.com>
2015-05-18 13:18:26 -0700

    createBranch update

--- serpent/call_contract.py
+++ serpent/call_contract.py
@@ -14,15 +14,16 @@ contracts = {
     'expiringEvents' : '0x61d90fd4c1c3502646153003ec4d5c177de0fb58',
     'fxpFunctions' : '0xdaf26192091449d14c03026f79272e410fce0908',
     'markets' : '0x2303f6b69e1d7662320819af027d88a9e350ebfb',
-    'reporting' : '0xd1f7f020f24abca582366ec80ce2fef6c3c22233',
+    'reporting' : '0xead0c9a9bd9546f476337a79e3d9bc8875241e61',
     'createEvent' : '0xcae6d5912033d66650894e2ae8c2f7502eaba15c',
     'createMarket' : '0x0568c631465eca542affb4bd3c72d1d2ee222c06',
     'checkQuorum' : '0x4eaa7a8b00107bbc11909e327e163b067fd3cfb9',
     'buy&sellShares' : '0xb8555091be5c8b8fc77449bb203717959079c29a',
-    'createBranch' : '0x5c955b31ac72c83ffd7562aed4e2100b2ba09a3b',
+    'createBranch' : '0x00298dd4fcae1b6063e963048716aeeed853edb6',
     'sendReputation' : '0x049487d32b727be98a4c8b58c4eab6521791f288',
     'transferShares' : '0x78da82256f5775df22eee51096d27666772b592d',
     'makeReports' : '0x32bfb5724874b209193aa0fca45b1f337b27e9b5',
+    'dispatch' : '0x662f95de5a6c500de0b35b73f4b48d740d267482',
 }
 
 def get_sym(arg):
