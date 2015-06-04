08ddc34416016ca540b5805c441be7084c629634
Chris Calderon <chris-da-dev@augur.net>
2015-06-04 13:59:26 -0700

    updates to rpc code and price fix

--- pyrpctools/rpc_server.py
+++ pyrpctools/rpc_server.py
@@ -1,4 +1,19 @@
 import json
-import http
+from time import strftime, gmtime
+from http import simple_server
 from ethereum import tester
+
+STATE = tester.state()
+RESPONSE = '''\
+HTTP/1.1 200 OK\r
+Date: %(date)s\r
+Content-Type: application/json\r
+Content-Length: %(length)d\r
+\r
+%(body)s'''
+def get_time():
+    return strftime('%a, %d %b %Y %T GMT', gmtime()) 
+
+def json_rpc_handler(request):
+    pass
 
