2322982c750cc2bca05364fd7425351ecd138843
Chris Calderon <chris-da-dev@augur.net>
2015-06-05 17:52:35 -0700

    updated call_contract.py and load_contract.py

--- load_contract.py
+++ load_contract.py
@@ -4,23 +4,59 @@ from collection import defaultdict
 import os
 import sys
 import json
-from load_contracts import get_fullname, broadcast_code
 
 RPC = RPC_Client(default='GETH')
 COINBASE = RPC.eth_coinbase()['result']
+TRIES = 10
+BLOCKTIME = 12
+SRCPATH = 'src'
+
+def memoize(func):
+    memo = {}
+    def new_func(x):
+        if x in memo:
+            return memo[x]
+        result = func(x)
+        memo.__setitem__(x, result)
+        return result
+    new_func.__name__ = func.__name__
+    return new_func
+
+@memoize
+def get_fullname(name):
+    '''
+    Takes a short name from an import statement and
+    returns a real path to that contract.
+    '''
+    for directory, subdirs, files in os.walk(SRCPATH):
+        for f in files:
+            if f[:-3] == name:
+                return os.path.join(directory, f)
+    raise ValueError('No such name: '+name)
+
+def broadcast_code(evm):
+    '''Sends compiled code to the network, and returns the address.'''
+    address = RPC.eth_sendTransaction(sender=COINBASE, data=evm, gas=GAS)['result']
+    tries = 0
+    while tries < TRIES:
+        check = RPC.eth_getCode(address)['result']
+        if check != '0x':
+            return address
+        time.sleep(BLOCKTIME)
+        tries += 1
+    raise ValueError('CODE COULD NOT GET ON CHAIN!!!')
 
 def build_dependencies():
-    # Assumes all code is in src
     deps = defaultdict(list)
-    for d, s, fs in os.walk('src'):
-        for f in fs:
-            for line in open(os.path.join(d, f)):
+    for directory, subdirs, files in os.walk(SRCPATH):
+        for f in files:
+            for line in open(os.path.join(directory, f)):
                 if line.startwith('import'):
                     name = line.split(' ')[1]
                     deps[name].append(f[:-3])
     return deps
 
-def compile(fullname, deps):
+def translate_code(fullname):
     new_code = []
     for line in open(fullname):
         line = line.rstrip()
@@ -32,7 +68,10 @@ def compile(fullname, deps):
             new_code.append(sub + ' = ' + info['address'])
         else:
             new_code.append(line)
-    new_code = '\n'.join(new_code)
+    return '\n'.join(new_code)
+
+def compile(fullname, deps):
+    new_code = translate_code(fullname)
     evm = '0x' + serpent.compile(new_code).encode('hex')
     new_address = broadcast_code(evm)
     short_name = os.path.split(fullname)[-1][:-3]
@@ -42,7 +81,7 @@ def compile(fullname, deps):
     DB[short_name] = json.dumps(new_info)
     if short_name in deps:
         for dep in deps[short_name]:
-            dep_fullname = get_full_name(dep)
+            dep_fullname = get_fullname(dep)
             compile(dep_fullname, deps)
 
 def main(contract_path):
