a3babb5ad510c085d76d7d01122c9ec9db7e0fe9
Jack Peterson <jack@tinybike.net>
2015-06-13 22:44:09 -0700

    Added absolute db path

--- pyrpctools/__init__.py
+++ pyrpctools/__init__.py
@@ -1,3 +1,6 @@
+import os
 from rpc_client import RPC_Client
 import leveldb
-DB = leveldb.LevelDB('build.dat')
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+DB = leveldb.LevelDB(os.path.join(PATH, os.pardir, 'build.dat'))
