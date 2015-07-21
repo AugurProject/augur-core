7f3cb114bd9fae0f767564f355f2e799904968e7
Jack Peterson <jack@tinybike.net>
2015-07-20 17:40:28 -0700

    Added contract address argument to call_contract.js

--- call_contract.js
+++ call_contract.js
@@ -2,11 +2,11 @@
 /**
  * Augur contract method caller.
  *
- * Syntax: node call_contract.js <call/send> <method name> <arguments>
+ * Syntax: node call_contract.js <call/send> <contract address> <method name> <arguments>
  *
  * Examples:
- * node call_contract.js call getRepBalance 1010101 0x63524e3fe4791aefce1e932bbfb3fdf375bfad89
- * node call_contract.js send sendCash 0x6fc0a64e2dce367e35417bfd1568fa35af9f3e4b 10
+ * node call_contract.js call 0xc0b05fa75a4b4fbb8e7a2b9e8b08d0b8fbb39f49 getRepBalance 1010101 0x63524e3fe4791aefce1e932bbfb3fdf375bfad89
+ * node call_contract.js send 0x0cc139a358642026c5ae6ade3ed28460f691db4d sendCash 0x6fc0a64e2dce367e35417bfd1568fa35af9f3e4b 10
  *
  * @author Jack Peterson (jack@tinybike.net)
  */
@@ -26,9 +26,10 @@ function echo(label, output) {
     }
 }
 
-var tx = Augur.tx[process.argv[3]];
+var tx = Augur.tx[process.argv[4]];
 tx.send = process.argv[2] === "send";
-tx.params = process.argv.slice(4);
+tx.to = process.argv[3];
+tx.params = process.argv.slice(5);
 
 echo("TX:", tx);
 
