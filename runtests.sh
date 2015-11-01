cbd27d8721a896fb5488c989600ce1e379b74464
Jack Peterson <jack@tinybike.net>
2015-11-01 01:16:42 -0800

    Added fixedpoint tests to build script

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 python tests/serpent_tests/array_delete_test.py
 python tests/serpent_tests/save_load_tests.py
+
+python tests/fixedpoint_tests/lagrange_math.py
+python tests/fixedpoint_tests/fixedpoint_test.py
+
 py.test tests/accounts.py --doctest-modules -v
