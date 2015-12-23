a957d14c0ce369e5dd2ec3aeece6ad8a365b1abc
Joseph Krug <joeykrug@gmail.com>
2015-12-23 02:11:52 +0000

    added ether tests

--- src/python_serpent_tests.py
+++ src/python_serpent_tests.py
@@ -6,6 +6,7 @@ def cash_tests():
     t.gas_limit = 100000000
     s = t.state()
     c = s.abi_contract('data_api/cash.se')
+    
     c.initiateOwner(111)
     c.setCash(111, 10)
     gas_use(s)
@@ -22,6 +23,22 @@ def cash_tests():
     assert(c.balance(111)==20), "Send from broken"
     assert(c.balance(101)==1), "Send from broken"
     gas_use(s)
+    print "CASH OK"
+    
+def ether_tests():
+    global initial_gas
+    initial_gas = 0
+    t.gas_limit = 100000000
+    s = t.state()
+    c = s.abi_contract('data_api/ether.se')
+    assert(c.depositEther(value=5)==5), "Unsuccessful eth deposit"
+    assert(c.withdrawEther(111, 500)==0), "Printed money out of thin air..."
+    assert(c.withdrawEther(111, 5)==1), "Unsuccessful withdrawal"
+    print c.balance(111)
+    gas_use(s)
+    print "ETHER OK"
+    
+
 
 def gas_use(s):
     global initial_gas
@@ -32,3 +49,4 @@ def gas_use(s):
     
 if __name__ == '__main__':
     cash_tests()
+    ether_tests()
