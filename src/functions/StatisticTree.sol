4dbb1190541972509618e8a1f4c7e5cecc38d92e
Joseph Krug <joeykrug@gmail.com>
2016-02-17 03:54:06 +0000

    removed unnecessary files for mode

--- src/functions/StatisticTree.sol
+++
@@ -1,3 +0,0 @@
-# factory for order statistic trees
-# need ability to a) create a new tree and get its address (per event), insert elements, and calc. median
-# need appropriate whitelisting for OST.sol as well
