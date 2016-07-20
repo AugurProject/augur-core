34349662b5dcb0237290dfaa7ad386663375e73c
Jack Peterson <jack@tinybike.net>
2016-07-19 20:22:45 -0700

    Fixed get_remote_api

--- load_contracts/make_api.py
+++ load_contracts/make_api.py
@@ -71,7 +71,7 @@ def get_remote_api(api_url):
     response = requests.get(api_url)
     if response.status_code == 200:
         try:
-            return response.json()
+            return response.json()["functions"]
         except Exception as exc:
             print(exc)
 
