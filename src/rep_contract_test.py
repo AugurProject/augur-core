eda43de7b8df772a1ccf37c12786c8ec6a7553a6
Joseph Krug <joeykrug@gmail.com>
2016-09-16 03:52:50 +0000

    repcontract fix to comply w/ etherscan erc20 in addition to just mis

--- src/rep_contract_test.py
+++ src/rep_contract_test.py
@@ -4,14 +4,14 @@ t.gas_limit = 4100000
 s = t.state()
 d = s.abi_contract('test.se')
 c = s.abi_contract('repContract.se')
-amounts = [int(random.random()*x*10**20) for x in range(1, 11)]
-remaining = 11000000*10**20 - sum(amounts)
+amounts = [int(random.random()*x*10**18) for x in range(1, 11)]
+remaining = 11000000*10**18 - sum(amounts)
 amounts[0] += remaining
 accounts = [d.getSender(sender=eval('t.k'+str(x))) for x in range(10)]
 c.setSaleDistribution(accounts, amounts)
 assert(c.getSeeded())
-assert(c.getDecimals()==20)
-assert(c.totalSupply()==11000000*10**20)
+assert(c.decimals()==18)
+assert(c.totalSupply()==11000000*10**18)
 for x in range(10):
 	assert(c.balanceOf(accounts[x])==amounts[x])
 for x in range(5):
@@ -22,4 +22,4 @@ for x in range(5):
 	assert(c.balanceOf(accounts[x+5])==(amounts[x]+amounts[x+5]))
 for x in range(5, 10):
 	assert(c.transfer(0, (amounts[x]+amounts[x-5]), sender=eval('t.k'+str(x))))
-assert(c.balanceOf(0)==11000000*10**20)
+assert(c.balanceOf(0)==11000000*10**18)
