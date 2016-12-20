fce2f8b46ab1e3d185ece15cf5bebf74a2b464bb
Jack Peterson <jack@tinybike.net>
2016-12-20 11:02:48 -0800

    Split reset_private_chain into two scripts

--- load_contracts/reset_private_chain.sh
+++ load_contracts/reset_private_chain.sh
@@ -15,12 +15,3 @@ $AUGURJS/scripts/new-contracts.js
 $AUGURJS/scripts/canned-markets.js
 sleep 5
 kill $gethpid
-ssh jack@45.33.62.72 sudo service geth stop
-ssh jack@45.33.62.72 rm -Rf /home/jack/.ethereum-9000/geth/chaindata
-scp -rp $HOME/.ethereum-9000/geth/chaindata jack@45.33.62.72:/home/jack/.ethereum-9000/geth/chaindata
-ssh jack@45.33.62.72 sudo service geth start
-ssh jack@45.33.62.72 sudo service augur stop
-ssh jack@45.33.62.72 rm -Rf /home/jack/augur/build
-scp -rp $HOME/src/augur/build jack@45.33.62.72:/home/jack/augur/build
-ssh jack@45.33.62.72 cp /home/jack/augur/src/env-9000.json /home/jack/augur/build/env.json
-ssh jack@45.33.62.72 sudo service augur start
