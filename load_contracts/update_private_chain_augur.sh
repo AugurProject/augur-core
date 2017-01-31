f5019f3e9844012e7914a6a050f1a0f6eab16c28
Jack Peterson <jack@tinybike.net>
2017-01-31 03:53:10 -0800

    Reverted private chain update script

--- load_contracts/update_private_chain_augur.sh
+++ load_contracts/update_private_chain_augur.sh
@@ -2,10 +2,6 @@
 
 ssh jack@45.33.62.72 sudo service augur stop
 ssh jack@45.33.62.72 rm -Rf /home/jack/augur/build
-ssh jack@45.33.62.72 git stash
-ssh jack@45.33.62.72 git fetch --all
-ssh jack@45.33.62.72 git reset --hard origin/master
-ssh jack@45.33.62.72 yarn
 scp -rp $HOME/src/augur/build jack@45.33.62.72:/home/jack/augur/build
 ssh jack@45.33.62.72 cp /home/jack/augur/src/env-9000.json /home/jack/augur/build/config/env.json
 ssh jack@45.33.62.72 sudo service augur start
