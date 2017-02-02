b79279d070f8f9cc6ed641f3fcfb1d930b21affa
Jack Peterson <jack@tinybike.net>
2017-02-02 12:38:52 -0800

    Fixed update-private-chain-and-augur script; updatednetwork 3 build.json

--- load_contracts/upload_private_chain.sh
+++ load_contracts/upload_private_chain.sh
@@ -11,6 +11,6 @@ ssh jack@45.33.62.72 sudo service geth start
 ssh jack@45.33.62.72 sudo service augur stop
 ssh jack@45.33.62.72 rm -Rf /home/jack/augur/build
 scp -rp $HOME/src/augur/build jack@45.33.62.72:/home/jack/augur/build
-ssh jack@45.33.62.72 cp /home/jack/augur/src/env-9000.json /home/jack/augur/build/env.json
+ssh jack@45.33.62.72 cp /home/jack/augur/src/env-9000.json /home/jack/augur/build/config/env.json
 ssh jack@45.33.62.72 sudo service augur start
 
