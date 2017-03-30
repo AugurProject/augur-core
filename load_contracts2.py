65285dc469ba90a5ffd402d914335c9b6d4836b5
Chris Calderon <calderonchristian73@gmail.com>
2017-03-29 21:04:40 -0700

    Converted tabs to spaces

--- load_contracts2.py
+++ load_contracts2.py
@@ -29,10 +29,10 @@ import shutil
 import sys
 
 if (3, 0) <= sys.version_info:
-	_old_mk_signature = serpent.mk_signature
-	def mk_signature(code):
-		return _old_mk_signature(code).decode()
-	serpent.mk_signature = mk_signature
+    _old_mk_signature = serpent.mk_signature
+    def mk_signature(code):
+        return _old_mk_signature(code).decode()
+    serpent.mk_signature = mk_signature
 
 PYNAME = '[A-Za-z_][A-Za-z0-9_]*'
 IMPORT = re.compile('import ({0}) as ({0})'.format(PYNAME))
@@ -54,106 +54,106 @@ modes.add_argument('-U', '--update', help='Updates the externs in all the contra
 
 
 def find_files(source_dir, extension):
-	"""Makes a list of paths in the directory which end in the extenstion."""
-	paths = []
-	for entry in os.listdir(source_dir):
-		entry = os.path.join(source_dir, entry)
-		if os.path.isfile(entry) and entry.endswith(extension):
-			paths.append(entry)
-		elif os.path.isdir(entry):
-			paths.extend(find_files(entry, extension))
-		else:
-			continue
-	return paths
+    """Makes a list of paths in the directory which end in the extenstion."""
+    paths = []
+    for entry in os.listdir(source_dir):
+        entry = os.path.join(source_dir, entry)
+        if os.path.isfile(entry) and entry.endswith(extension):
+            paths.append(entry)
+        elif os.path.isdir(entry):
+            paths.extend(find_files(entry, extension))
+        else:
+            continue
+    return paths
 
 
 def strip_dependencies(serpent_file):
-	"""Separates dependency information from Serpent code in the file."""
-	dependencies = []
-	other_code = []
-	crud = []
-	done_with_crud = False
-	with open(serpent_file) as code:
-		for i, line in enumerate(code):
-			line = line.rstrip()
-			if line.startswith('#') and not done_with_crud:
-				crud.append(line)
-				continue
-			done_with_crud = True
+    """Separates dependency information from Serpent code in the file."""
+    dependencies = []
+    other_code = []
+    crud = []
+    done_with_crud = False
+    with open(serpent_file) as code:
+        for i, line in enumerate(code):
+            line = line.rstrip()
+            if line.startswith('#') and not done_with_crud:
+                crud.append(line)
+                continue
+            done_with_crud = True
 
-			if line.startswith('import'):
-				m = IMPORT.match(line)
-				if m is None:
-					print('SAD! weird import at line', i, 'in contract', path)
-					sys.exit(1)
-				dependencies.append((m.group(1), m.group(2)))
-			else:
-				other_code.append(line)
+            if line.startswith('import'):
+                m = IMPORT.match(line)
+                if m is None:
+                    print('SAD! weird import at line', i, 'in contract', path)
+                    sys.exit(1)
+                dependencies.append((m.group(1), m.group(2)))
+            else:
+                other_code.append(line)
 
-	other_code.append('')
-	crud.append('')
+    other_code.append('')
+    crud.append('')
 
-	return dependencies, '\n'.join(other_code), '\n'.join(crud)
+    return dependencies, '\n'.join(other_code), '\n'.join(crud)
 
 
 def imports_to_externs(source_dir, new_dir):
-	"""Translates code using import syntax to standard Serpent externs."""
-	source_dir = os.path.abspath(source_dir)
-	new_dir = os.path.abspath(new_dir)
+    """Translates code using import syntax to standard Serpent externs."""
+    source_dir = os.path.abspath(source_dir)
+    new_dir = os.path.abspath(new_dir)
 
-	shutil.copytree(source_dir, new_dir)
+    shutil.copytree(source_dir, new_dir)
 
-	serpent_files = find_files(new_dir, SERPENT_EXT)
+    serpent_files = find_files(new_dir, SERPENT_EXT)
 
-	source_map = {}
+    source_map = {}
 
-	for path in serpent_files:
-		name = os.path.basename(path).replace(SERPENT_EXT, '')
-		info = {}
-		info['dependencies'], info['stripped_code'], info['crud'] = strip_dependencies(path)
-		
-		with open(path, 'w') as temp:
-			temp.write(info['stripped_code'])
+    for path in serpent_files:
+        name = os.path.basename(path).replace(SERPENT_EXT, '')
+        info = {}
+        info['dependencies'], info['stripped_code'], info['crud'] = strip_dependencies(path)
+        
+        with open(path, 'w') as temp:
+            temp.write(info['stripped_code'])
 
-		extern_name = 'extern {}:'.format(name)
-		signature = serpent.mk_signature(path)
-		name_end = signature.find(':') + 1
-		signature = extern_name + signature[name_end:]
-		info['signature'] = signature
-		info['path'] = path
-		source_map[name] = info
+        extern_name = 'extern {}:'.format(name)
+        signature = serpent.mk_signature(path)
+        name_end = signature.find(':') + 1
+        signature = extern_name + signature[name_end:]
+        info['signature'] = signature
+        info['path'] = path
+        source_map[name] = info
 
-	lookup_fmt = '{} = Controller.lookup(\'{}\')'
-	for name in source_map:
-		info = source_map[name]
-		signatures = ['macro Controller: 0xC001D00D', 'extern Controller: [lookup:[int256]:int256, checkWhitelist:[int256]:int256]']
-		for oname, alias in info['dependencies']:
-			signatures.append('')
-			signatures.append(lookup_fmt.format(alias, oname))
-			signatures.append(source_map[oname]['signature'])
-		signatures.append('')
-		new_code = '\n'.join([info['crud']] + signatures + [info['stripped_code']])
-		path = info['path']
+    lookup_fmt = '{} = Controller.lookup(\'{}\')'
+    for name in source_map:
+        info = source_map[name]
+        signatures = ['macro Controller: 0xC001D00D', 'extern Controller: [lookup:[int256]:int256, checkWhitelist:[int256]:int256]']
+        for oname, alias in info['dependencies']:
+            signatures.append('')
+            signatures.append(lookup_fmt.format(alias, oname))
+            signatures.append(source_map[oname]['signature'])
+        signatures.append('')
+        new_code = '\n'.join([info['crud']] + signatures + [info['stripped_code']])
+        path = info['path']
 
-		with open(path, 'w') as f:
-			f.write(new_code)
+        with open(path, 'w') as f:
+            f.write(new_code)
 
 
 def update_externs(source_dir):
-	pass
+    pass
 
 
 def main():
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if args.translate:
-		imports_to_externs(args.srcdir, args.builddir)
-	elif args.update:
-		update_externs(args.srcdir)
-	else:
-		return 1
+    if args.translate:
+        imports_to_externs(args.srcdir, args.builddir)
+    elif args.update:
+        update_externs(args.srcdir)
+    else:
+        return 1
 
-	return 0
+    return 0
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
