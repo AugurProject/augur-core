7f7488ddd879297d51047711cb62b2669c7d66e9
Joseph Krug <joeykrug@gmail.com>
2017-04-18 23:52:19 +0000

    latest before merge

--- load_contracts2.py
+++ load_contracts2.py
@@ -56,6 +56,7 @@ STANDARD_EXTERNS = {
     'wallet': 'extern wallet: [initialize:[int256]:int256, setWinningOutcomeContractAddressInitialize:[int256,int256]:int256, transfer:[address,uint256]:int256]',
     'rateContract': 'extern rateContract: [rateFunction:[]:int256]',
     'forkResolveContract': 'extern forkResolveContract: [resolveFork:[]:int256]',
+    'shareTokens': 'extern shareTokens: [allowance:[address,address]:int256, approve:[address,uint256]:int256, balanceOf:[address]:int256, changeTokens:[int256,int256]:int256, getDecimals:[]:int256, getName:[]:int256, getSymbol:[]:int256, modifySupply:[int256]:int256, totalSupply:[]:int256, transfer:[address,uint256]:int256, transferFrom:[address,address,uint256]:int256]',
 }
 
 DEFAULT_RPCADDR = 'http://localhost:8545'
