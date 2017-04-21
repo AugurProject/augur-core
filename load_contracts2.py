6efb26aa84be5e34ee971b27bd08fa4d055d3af6
Joseph Krug <joeykrug@gmail.com>
2017-04-21 20:40:35 +0000

    separated create market into separate contracts

--- load_contracts2.py
+++ load_contracts2.py
@@ -396,7 +396,7 @@ class ContractLoader(object):
     """
     def __init__(self, source_dir, controller, special):
         self.__state = ethereum.tester.state()
-        ethereum.tester.gas_limit = 4000000
+        ethereum.tester.gas_limit = 4100000
         self.__contracts = {}
         self.__temp_dir = TempDirCopy(source_dir)
         self.__source_dir = source_dir
