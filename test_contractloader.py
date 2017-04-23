ce9970b1d650a59a8174c7ddcbf51f913eb4cf11
Joseph Krug <joeykrug@gmail.com>
2017-04-23 23:17:11 +0000

    fixed delegate calls

--- test_contractloader.py
+++ test_contractloader.py
@@ -1,11 +1,17 @@
 from load_contracts import ContractLoader
-import pickle
+import dill
+import cloudpickle
 c = ContractLoader('src', 'controller.se', ['mutex.se', 'cash.se', 'repContract.se'])
 c.recompile('test')
-output = open('data.pkl', 'wb')
-pickle.dump(c, output)
+
+output = open('data.dill', 'wb')
+dill.dump(c, output)
 output.close()
 
-pkl_file = open('data.pkl', 'wb')
-c = pickle.load(pkl_file)
-pkl_file.close()
+output = open('data.cp', 'wb')
+cloudpickle.dump(c, output)
+output.close()
+
+dill_file = open('data.dill', 'rb')
+c = dill.load(dill_file)
+dill_file.close()
