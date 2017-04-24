eef385f5748d6bb018cca2bf0e5e5543eb183b38
John Danz <John.Danz87@gmail.com>
2017-04-24 15:33:01 -0500

    after merging develop fix for whitelist issues, began working on filling out data_api unit tests

--- tests/data_api_tests.py
+++ tests/data_api_tests.py
@@ -14,23 +14,57 @@ TWO = 2*ONE
 HALF = ONE/2
 
 def test_backstops(c, s, t):
-    event = 1234
-    assert(c.getDisputedOverEthics(event) == 0), "disputedOverEthics isn't defaulted to 0"
-    c.setDisputedOverEthics(event)
+    event1 = 1234
+    event2 = 5678
+    address1 = long(t.a1.encode("hex"), 16)
+    address2 = long(t.a2.encode("hex"), 16)
 
-def test_branches(c, s, t):
-    branch = 1010101
-    def test_incrementPeriod():
-        print "begin branches.incrementPeriod tests"
-        print c.getVotePeriod(branch)
-        c.incrementPeriod(branch)
-        print c.getVotePeriod(branch)
-        print "branches.incrementPeriod tests complete"
-    test_incrementPeriod()
+    def test_disputedOverEthics():
+        assert(c.getDisputedOverEthics(event1) == 0), "disputedOverEthics isn't defaulted to 0"
+        c.setDisputedOverEthics(event1)
+        assert(c.getDisputedOverEthics(event1) == 1), "setDisputedOverEthics didn't set event properly"
+        try:
+            raise Exception(c.setDisputedOverEthics(event1, sender=t.k1))
+        except Exception as exc:
+            assert(isinstance(exc, t.TransactionFailed)), "setDisputedOverEthics should throw if msg.sender isn't whitelisted"
+
+    def test_forkBondPoster():
+        assert(c.getForkBondPoster(event1) == 0), "forkBondPoster isn't defaulted to 0"
+        assert(c.setForkBondPoster(event1, t.a1) == 1), "Didn't set forkBondPoster correctly"
+        assert(c.getForkBondPoster(event1) == address1), "Didn't set the forkBondPoster to the expected address"
+        try:
+            raise Exception(c.setForkBondPoster(event1, t.a2, sender=t.k1))
+        except Exception as exc:
+            assert(isinstance(exc, t.TransactionFailed)), "setForkBondPoster should throw if msg.sender isn't whitelisted"
+
+    def test_forkedOverEthicality():
+        assert(c.getForkedOverEthicality(event1) == 0), "forkedOverEthicality isn't defaulted to 0"
+        assert(c.setForkedOverEthicality(event1) == 1), "setForkedOverEthicality isn't returning 1 as expected when msg.sender is whitelisted"
+        assert(c.getForkedOverEthicality(event1) == 1), "forkedOverEthicality wasn't set correctly"
+        try:
+            assert(c.getForkedOverEthicality(event2) == 0), "forkedOverEthicality isn't defaulted to 0"
+            raise Exception(c.setForkedOverEthicality(event2, sender=t.k1))
+        except Exception as exc:
+            assert(isinstance(exc, t.TransactionFailed)), "setForkedOverEthicality should throw if msg.sender isn't whitelisted"
+
+    test_disputedOverEthics()
+    test_forkBondPoster()
+    test_forkedOverEthicality()
+
+    print "data_api/backstops.se unit tests completed"
+
+# def test_branches(c, s, t):
+#     branch = 1010101
+#     def test_incrementPeriod():
+#
+#         assert(c.getVotePeriod(branch) == 94064891)
+#         c.incrementPeriod(branch)
+#         assert(c.getVotePeriod(branch) == 95065892)
+#     test_incrementPeriod()
 
 if __name__ == '__main__':
     src = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'src')
-    contracts = ContractLoader(src, 'controller.se', ['mutex.se', 'cash.se', 'repContract.se'], '', 1)
+    contracts = ContractLoader(src, 'controller.se', ['mutex.se', 'cash.se', 'repContract.se'], '', 0)
     state = contracts.state
     t = contracts._ContractLoader__tester
     test_backstops(contracts.backstops, state, t)
