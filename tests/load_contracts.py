60356fcf07ab5b2e09538c4f4c4703f752bb29fd
Joseph Krug <joeykrug@gmail.com>
2017-04-26 05:00:40 +0000

    made gas limit closer to existing limit

--- tests/load_contracts.py
+++ tests/load_contracts.py
@@ -398,7 +398,7 @@ class ContractLoader(object):
     def __init__(self, source_dir, controller, special):
         self.__state = ethereum.tester.state()
         self.__tester = ethereum.tester
-        ethereum.tester.gas_limit = 4100000
+        ethereum.tester.gas_limit = 4200000
         self.__contracts = {}
         self.__temp_dir = TempDirCopy(source_dir)
         self.__source_dir = source_dir
