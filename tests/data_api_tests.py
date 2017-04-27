da54f000e0b3985a4d692f647667fa89a27db467
John Danz <John.Danz87@gmail.com>
2017-04-27 17:28:27 -0500

    changed the way increaseDenominator and decreaseDenominator work in data_api/consensusData.se. The change will cause them to throw if a negative amount is passed to the function instead of silently failing and returning 1.

--- tests/data_api_tests.py
+++ tests/data_api_tests.py
@@ -235,7 +235,7 @@ def test_backstops(contracts, s, t):
     test_roundTwo()
     test_roundTwoRefund()
     # test_misc()
-    print "data_api/backstops.se unit tests completed"
+    print("data_api/backstops.se unit tests completed")
 
 def test_branches(contracts, s, t):
     c = contracts.branches
@@ -284,11 +284,11 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "updateNumCurrencies should throw if msg.sender isn't whitelisted"
         #     assert(c.getNumCurrencies(branch1) == 1), "numCurrencies was modified when it shouldn't have been"
         # now with whitelist
-        assert(c.getNumCurrencies(branch1) == 1)
-        assert(c.updateNumCurrencies(branch1, 5) == 1)
-        assert(c.getNumCurrencies(branch1) == 5)
-        assert(c.updateNumCurrencies(branch1, 1) == 1)
-        assert(c.getNumCurrencies(branch1) == 1)
+        assert(c.getNumCurrencies(branch1) == 1), "numCurrenceies should be 1 at this point"
+        assert(c.updateNumCurrencies(branch1, 5) == 1), "updateNumCurrencies wasn't called successfully"
+        assert(c.getNumCurrencies(branch1) == 5), "numCurrencies should be 5 at this point"
+        assert(c.updateNumCurrencies(branch1, 1) == 1), "updateNumCurrencies wasn't called successfully"
+        assert(c.getNumCurrencies(branch1) == 1), "numCurrenceies should be 1 at this point"
         # add a currency
         # try:
         #     # should fail if not whitelisted...
@@ -298,11 +298,11 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "addCurrency should throw if msg.sender isn't whitelisted"
         #     assert(c.getNumCurrencies(branch1) == 1), "numCurrencies was modified when it shouldn't have been"
         # now with whitelist
-        assert(c.addCurrency(branch1, repAddr, rate, 0) == 1)
-        assert(c.getNumCurrencies(branch1) == 2)
-        assert(c.getBranchWallet(branch1, repAddr) != 0)
-        assert(c.getCurrencyByContract(branch1, repAddr) == 1)
-        assert(c.getCurrencyRate(branch1, repAddr) == rate)
+        assert(c.addCurrency(branch1, repAddr, rate, 0) == 1), "addCurreny wasn't successfully executed"
+        assert(c.getNumCurrencies(branch1) == 2), "numCurrenceies should be 2 at this point"
+        assert(c.getBranchWallet(branch1, repAddr) != 0), "There should be a wallet defined for the new currency"
+        assert(c.getCurrencyByContract(branch1, repAddr) == 1), "the new currency should be indexed at the 1 position"
+        assert(c.getCurrencyRate(branch1, repAddr) == rate), "the new currency should have returned the expected rate"
         # disable and reactivate currency
         # try:
         #     # should fail if not whitelisted...
@@ -312,9 +312,9 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "disableCurrency should throw if msg.sender isn't whitelisted"
         #     assert(c.getCurrencyActive(branch1, repAddr) == 1), "REP currency should be active still"
         # now with whitelist
-        assert(c.getCurrencyActive(branch1, repAddr) == 1)
-        assert(c.disableCurrency(branch1, repAddr) == 1)
-        assert(c.getCurrencyActive(branch1, repAddr) == 0)
+        assert(c.getCurrencyActive(branch1, repAddr) == 1), "the new currency should be active"
+        assert(c.disableCurrency(branch1, repAddr) == 1), "disableCurrency wasn't successfully executed"
+        assert(c.getCurrencyActive(branch1, repAddr) == 0), "the new currency should be inactive"
         # try:
         #     # should fail if not whitelisted...
         #     assert(c.getCurrencyActive(branch1, repAddr) == 0), "REP currency should be inactive at this point"
@@ -323,10 +323,10 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "reactivateCurrency should throw if msg.sender isn't whitelisted"
         #     assert(c.getCurrencyActive(branch1, repAddr) == 0), "REP currency should be inactive still"
         # now with whitelist
-        assert(c.reactivateCurrency(branch1, repAddr) == 1)
-        assert(c.getCurrencyActive(branch1, repAddr) == 1)
+        assert(c.reactivateCurrency(branch1, repAddr) == 1), "reactivateCurrency wasn't called successfully"
+        assert(c.getCurrencyActive(branch1, repAddr) == 1), "the new currency should be active again"
         # modify a currencyRate
-        assert(c.getCurrencyRate(branch1, repAddr) == rate)
+        assert(c.getCurrencyRate(branch1, repAddr) == rate), "the new currency's rate should be the rate it was added at"
         # try:
         #     # should fail if not whitelisted...
         #     assert(c.getCurrencyRate(branch1, repAddr) == rate), "REP currency should have a rate of 1"
@@ -335,8 +335,8 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "updateCurrencyRate should throw if msg.sender isn't whitelisted"
         #     assert(c.getCurrencyRate(branch1, repAddr) == rate), "REP currency should still have a rate of 1"
         # now with whitelist
-        assert(c.updateCurrencyRate(branch1, repAddr, (rate + 1), 0) == 1)
-        assert(c.getCurrencyRate(branch1, repAddr) == (rate + 1))
+        assert(c.updateCurrencyRate(branch1, repAddr, (rate + 1), 0) == 1), "updateCurrencyRate wasn't executed successfully"
+        assert(c.getCurrencyRate(branch1, repAddr) == (rate + 1)), "New currency's rate wasn't it's previous rate + 1 as it should be at this point"
         # replace currency
         # try:
         #     # should fail if not whitelisted...
@@ -346,9 +346,9 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "replaceCurrency should throw if msg.sender isn't whitelisted"
         #     assert(c.getCurrencyRate(branch1, repAddr) == (rate + 1)), "REP currency should still have a modified rate"
         # now with whitelist
-        assert(c.replaceCurrency(branch1, 1, repAddr, rate, 0) == 1)
-        assert(c.getCurrencyRate(branch1, repAddr) == rate)
-        assert(c.getNumCurrencies(branch1) == 2)
+        assert(c.replaceCurrency(branch1, 1, repAddr, rate, 0) == 1), "replaceCurrency wasn't executed successfully"
+        assert(c.getCurrencyRate(branch1, repAddr) == rate), "the new currency's rate should be it's original rate again"
+        assert(c.getNumCurrencies(branch1) == 2), "numCurrencies should be 2 at this point"
         # remove last currency
         # try:
         #     # should fail if not whitelisted...
@@ -358,52 +358,134 @@ def test_branches(contracts, s, t):
         #     assert(isinstance(exc, t.TransactionFailed)), "removeLastCurrency should throw if msg.sender isn't whitelisted"
         #     assert(c.getBranchCurrency(branch1, 1) == repAddr), "REP currency should still be defined"
         # now with whitelist
-        assert(c.getBranchCurrency(branch1, 1) == repAddr)
-        assert(c.getCurrencyRate(branch1, repAddr) == rate)
-        assert(c.removeLastCurrency(branch1) == 1)
-        assert(c.getNumCurrencies(branch1) == 1)
-        assert(c.getCurrencyByContract(branch1, repAddr) == 0)
-        assert(c.getBranchWallet(branch1, repAddr) == 0)
-        assert(c.getBranchCurrency(branch1, 1) == 0)
-        assert(c.getCurrencyRate(branch1, repAddr) == 0)
+        assert(c.getBranchCurrency(branch1, 1) == repAddr), "branch currency at position 1 should be the new currency"
+        assert(c.getCurrencyRate(branch1, repAddr) == rate), "the rate for the new currency should be the original rate"
+        assert(c.removeLastCurrency(branch1) == 1), "removeLastCurrency wasn't executed successfully"
+        assert(c.getNumCurrencies(branch1) == 1), "numCurrencies should be 1 at this point"
+        assert(c.getCurrencyByContract(branch1, repAddr) == 0), "the new currency should have been removed"
+        assert(c.getBranchWallet(branch1, repAddr) == 0), "The new currency's wallet shouldn't be defined"
+        assert(c.getBranchCurrency(branch1, 1) == 0), "branch currency at position 1 should be empty"
+        assert(c.getCurrencyRate(branch1, repAddr) == 0), "new Currency Rate should return 0 since new currency should be removed at this point"
 
     def test_branching():
         # test branch addition and confirm expected state
         cashAddr = long(contracts.cash.address.encode("hex"), 16)
-        assert(c.getNumBranches() == 1)
-        assert(c.getBranches() == [branch1])
-        assert(c.initializeBranch(branch2, period2, 15, 200000000000000000, period1, branch1, cashAddr, 0) == 1)
-        assert(c.getNumBranches() == 2)
-        assert(c.getBranches() == [branch1, branch2])
-        assert(c.getParent(branch2) == branch1)
-        assert(c.getParentPeriod(branch2) == period1)
-        assert(c.getVotePeriod(branch2) == period2)
-        assert(c.incrementPeriod(branch2) == 1)
-        assert(c.getVotePeriod(branch2) == (period2 + 1))
+        assert(c.getNumBranches() == 1), "numBranches should be 1"
+        assert(c.getBranches() == [branch1]), "getBranches should return only 1 branch"
+        assert(c.initializeBranch(branch2, period2, 15, 200000000000000000, period1, branch1, cashAddr, 0) == 1), "initializeBranch wasn't executed successfully"
+        assert(c.getNumBranches() == 2), "numBranches should be 2"
+        assert(c.getBranches() == [branch1, branch2]), "getBranches should return 2 branches"
+        assert(c.getParent(branch2) == branch1), "new branch parent should be the original branch"
+        assert(c.getParentPeriod(branch2) == period1), "parentPeriod should be original branch's period"
+        assert(c.getVotePeriod(branch2) == period2), "branch 2 didn't return the execpted votePeriod"
+        assert(c.incrementPeriod(branch2) == 1), "incrementPeriod wasn't executed successfully"
+        assert(c.getVotePeriod(branch2) == (period2 + 1)), "branch 2 should have it's period incremented by 1"
 
     def test_edits():
         # test modifying the branches in various ways
-        assert(c.getNumMarketsBranch(branch1) == 0)
-        assert(c.getMarketIDsInBranch(branch1, 0, 5) == [0, 0, 0, 0, 0])
-        assert(c.addMarketToBranch(branch1, 1) == 1)
-        assert(c.getNumMarketsBranch(branch1) == 1)
-        assert(c.getMarketIDsInBranch(branch1, 0, 5) == [1, 0, 0, 0, 0])
+        assert(c.getNumMarketsBranch(branch1) == 0), "numMarkets should be 0"
+        assert(c.getMarketIDsInBranch(branch1, 0, 5) == [0, 0, 0, 0, 0]), "getting the first 5 markets in branch 1 should return an array of 0s"
+        assert(c.addMarketToBranch(branch1, 10987) == 1), "addMarketToBranch wasn't executed successfully"
+        assert(c.getNumMarketsBranch(branch1) == 1), "numMarkets should be 1"
+        assert(c.getMarketIDsInBranch(branch1, 0, 5) == [10987, 0, 0, 0, 0]), "getting the first 5 markets in branch 1 should return an array with 1 market set and 4 empty markets"
         # set forkperiod
-        assert(c.getForkPeriod(branch1) == 0)
-        assert(c.getForkTime(branch1) == 0)
-        assert(c.setForkPeriod(branch1) == 1)
-        assert(c.getForkPeriod(branch1) == c.getVotePeriod(branch1))
-        assert(c.getForkTime(branch1) == s.block.timestamp)
+        assert(c.getForkPeriod(branch1) == 0), "forkPeriod for branch 1 should be 0"
+        assert(c.getForkTime(branch1) == 0), "forkTime for branch 1 should be 0"
+        assert(c.setForkPeriod(branch1) == 1), "setForkPeriod wasn't executed successfully"
+        assert(c.getForkPeriod(branch1) == c.getVotePeriod(branch1)), "branch 1 should have it's forkPeriod set to it's vote Period"
+        assert(c.getForkTime(branch1) == s.block.timestamp), "branch 1 should have it's forkTime set to block.timestamp"
         # most recent child
-        assert(c.getMostRecentChild(branch2) == 0)
-        assert(c.setMostRecentChild(branch2, 1) == 1)
-        assert(c.getMostRecentChild(branch2) == 1)
+        assert(c.getMostRecentChild(branch2) == 0), "branch 2 most recent child should be 0"
+        assert(c.setMostRecentChild(branch2, 1) == 1), "setMostRecentChild wasn't executed successfully"
+        assert(c.getMostRecentChild(branch2) == 1), "branch 2 should have a most recent child set to 1 at this point"
 
     test_defaults()
     test_currency()
     test_branching()
     test_edits()
-    print "data_api/branches.se unit tests completed"
+    print("data_api/branches.se unit tests completed")
+
+def test_consensusData(contracts, s, t):
+    c = contracts.consensusData
+    branch1 = 1010101
+    period1 = contracts.branches.getVotePeriod(branch1)
+    event1 = 1234
+    address2 = long(t.a2.encode("hex"), 16)
+    cashAddr = long(contracts.cash.address.encode("hex"), 16)
+
+    def test_baseReporting():
+        assert(c.getBaseReportersLastPeriod(branch1) == 6), "default should be 6"
+        assert(c.setBaseReportersLastPeriod(branch1, 10) == 1), "Didn't successfully execute setBaseReportersLastPeriod"
+        assert(c.getBaseReportersLastPeriod(branch1) == 10), "baseReportersLastPeriod should be 10 at this point"
+        assert(c.setBaseReportersLastPeriod(branch1, 6) == 1), "setBaseReportersLastPeriod wasn't executed successfully"
+        assert(c.getBaseReportersLastPeriod(branch1) == 6), "baseReportersLastPeriod should be 6 at this point"
+
+    def test_slashed():
+        assert(c.getSlashed(branch1, period1, address2) == 0), "slashed should be 0 by default"
+        assert(c.setSlashed(branch1, period1, address2) == 1), "setSlashed wasn't executed successfully"
+        assert(c.getSlashed(branch1, period1, address2) == 1), "slashed Should be 1 at this point"
+
+    def test_feeFirst():
+        assert(c.getFeeFirst(branch1, period1) == 0), "feeFirst should be 0 by default"
+        assert(c.setFeeFirst(branch1, period1, 100) == 1), "setFeeFirst wasn't executed successfully"
+        assert(c.getFeeFirst(branch1, period1) == 100), "feeFirst should be set to 100 at this point"
+        assert(c.setFeeFirst(branch1, period1, 0) == 1), "setFeeFirst wasn't executed successfully"
+        assert(c.getFeeFirst(branch1, period1) == 0), "feeFirst should be 0 at this point"
+
+    def test_periodBalance():
+        assert(c.getPeriodBalance(branch1, period1) == 0), "periodBalance should be 0 by default"
+        assert(c.setPeriodBalance(branch1, period1, 100) == 1), "setPeriodBalance wasn't executed successfully"
+        assert(c.getPeriodBalance(branch1, period1) == 100), "periodBalance should be set to 100"
+        assert(c.setPeriodBalance(branch1, period1, 0) == 1), "setPeriodBalance wasn't executed successfully"
+        assert(c.getPeriodBalance(branch1, period1) == 0), "periodBalance should be 0 at this point"
+
+    def test_denominator():
+        assert(c.getDenominator(branch1, period1) == 0), "denominator should be 0 by default"
+        assert(c.increaseDenominator(branch1, period1, 200) == 1), "increaseDenominator wasn't executed successfully"
+        assert(c.getDenominator(branch1, period1) == 200), "denominator should be 200 at this point"
+        assert(c.decreaseDenominator(branch1, period1, 200) == 1), "decreaseDenominator wasn't executed successfully"
+        assert(c.getDenominator(branch1, period1) == 0), "denominator should be 0 at this point"
+
+    def test_collections():
+        # $currency Fees
+        assert(c.getFeesCollected(branch1, address2, period1, cashAddr) == 0), "feesCollected for cash should be set to 0 by default"
+        assert(c.setFeesCollected(branch1, address2, period1, cashAddr) == 1), "setFeesCollected wasn't executed successfully"
+        assert(c.getFeesCollected(branch1, address2, period1, cashAddr) == 1), "feesCollected for cash should be set to 1"
+        # REP
+        assert(c.getRepCollected(branch1, address2, period1) == 0), "repCollected should be set to 0 by default"
+        assert(c.setRepCollected(branch1, address2, period1) == 1), "setRepCollected wasn't executed successfully"
+        assert(c.getRepCollected(branch1, address2, period1) == 1), "repCollected should be set to 1"
+
+    def test_penalization():
+        assert(c.getPenalized(branch1, period1, address2, event1) == 0), "penalized should be set to 0 by default"
+        assert(c.setPenalized(branch1, period1, address2, event1) == 1), "setPenalized wasn't executed successfully"
+        assert(c.getPenalized(branch1, period1, address2, event1) == 1), "penalized should be set to 1"
+
+        assert(c.getPenalizedNum(branch1, period1, address2) == 0), "penalizedNum should be 0 by default"
+        assert(c.increasePenalizedNum(branch1, period1, address2, 1) == 1), "increasePenalizedNum wasn't executed successfully"
+        assert(c.getPenalizedNum(branch1, period1, address2) == 1), "penalizedNum should be 1"
+
+        assert(c.getPenalizedUpTo(branch1, address2) == 0), "penalizedUpTo should be set to 0 by default"
+        assert(c.setPenalizedUpTo(branch1, address2, (period1 - 5)) == 1), "setPenalizedUpTo wasn't executed successfully"
+        assert(c.getPenalizedUpTo(branch1, address2) == (period1 - 5)), "penalizedUpTo wasn't set to the expected period"
+        assert(c.getRepRedistributionDone(branch1, address2) == 0), "repRedistributionDone should return 0 when we aren't caught up to the branch votePeriod"
+        assert(c.setPenalizedUpTo(branch1, address2, (period1 - 1)) == 1), "setPenalizedUpTo wasn't executed successfully"
+        assert(c.getPenalizedUpTo(branch1, address2) == (period1 - 1)), "penalizedUpTo isn't the votePeriod - 1 as expected"
+        assert(c.getRepRedistributionDone(branch1, address2) == 1), "getRepRedistributionDone should return 1 when we are caught up to the branch votePeriod"
+
+        assert(c.getNotEnoughPenalized(branch1, address2, period1) == 0), "notEnoughReportsPenalized should be set to 0 by default"
+        assert(c.setNotEnoughPenalized(branch1, address2, period1) == 1), "setNotEnoughPenalized wasn't executed successfully"
+        assert(c.getNotEnoughPenalized(branch1, address2, period1) == 1), "notEnoughReportsPenalized should be set to 1"
+
+    # TODO: setRefund - doRefund
+    test_baseReporting()
+    test_slashed()
+    test_feeFirst()
+    test_periodBalance()
+    test_denominator()
+    test_collections()
+    test_penalization()
+    print("data_api/consensusData.se unit tests completed")
 
 if __name__ == '__main__':
     src = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'src')
@@ -413,7 +495,7 @@ if __name__ == '__main__':
     print "BEGIN TESTING DATA_API"
     test_backstops(contracts, state, t)
     test_branches(contracts, state, t)
-    # data_api/consensusData.se
+    test_consensusData(contracts, state, t)
     # data_api/events.se
     # data_api/expiringEvents.se
     # data_api/fxpFunctions.se
