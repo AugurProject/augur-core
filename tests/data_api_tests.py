34eec6942534214502f8c4f96647842cd10e7cac
John Danz <John.Danz87@gmail.com>
2017-04-29 09:35:29 -0500

    added tests for info.se

--- tests/data_api_tests.py
+++ tests/data_api_tests.py
@@ -658,6 +658,49 @@ def test_events(contracts, s, t):
     test_rejection()
     print("data_api/events.se unit tests completed")
 
+def test_info(contracts, s, t):
+    c = contracts.info
+    branch1 = 1010101
+    branch2 = 2020202
+    cashAddr = long(contracts.cash.address.encode("hex"), 16)
+    cashWallet = contracts.branches.getBranchWallet(branch1, cashAddr)
+    address0 = long(t.a0.encode("hex"), 16)
+    address1 = long(t.a1.encode("hex"), 16)
+    ONE = 10**18
+    expectedCreationFee = 10 * ONE
+
+    def test_defaults():
+        assert(c.getCreator(branch1) == address0), "creator of default branch wasn't the expected address"
+        assert(c.getDescription(branch1) == 'Root branch'), "description for default branch should be 'Root branch'"
+        assert(c.getDescriptionLength(branch1) == 11), "descriptionLength should be 11 by default"
+        assert(c.getCreationFee(branch1) == expectedCreationFee), "the creation fee for the default branch wasn't the expected creation fee (10 * ONE)"
+        assert(c.getCurrency(branch1) == 0), "the default currency for the default branch should be set to 0 by default"
+        assert(c.getWallet(branch1) == 0), "the default wallet for the default branch should be set to 0 by default"
+
+    def test_currency():
+        assert(c.getCurrency(branch1) == 0), "the default currency for the default branch should be set to 0 by default"
+        assert(c.getWallet(branch1) == 0), "the default wallet for the default branch should be set to 0 by default"
+        assert(c.setCurrencyAndWallet(branch1, cashAddr, cashWallet) == 1), "setCurrencyAndWallet wasn't executed successfully"
+        assert(c.getCurrency(branch1) == cashAddr), "currency for default branch didn't get set to the cash address as expected"
+        assert(c.getWallet(branch1) == cashWallet), "wallet for the default branch wasn't set to the cash wallet as expected"
+
+    def test_setInfo():
+        assert(c.setInfo(branch1, 'Hello World', address0, expectedCreationFee, cashAddr, cashWallet) == 0), "default branch already exists, so this shouldn't change anything and return 0"
+        assert(c.getDescription(branch1) == 'Root branch'), "Confirm that the description hasn't changed because setInfo should have failed and returned 0"
+        assert(c.getCreator(branch2) == 0), "branch2 shouldn't have been added to info yet and the creator should be set to 0."
+        assert(c.setInfo(branch2, 'Test Branch', address1, expectedCreationFee, cashAddr, cashWallet) == 1), "setInfo wasn't successfully executed when it should have been"
+        assert(c.getCreator(branch2) == address1), "creator of branch2 wasn't the expected address"
+        assert(c.getDescription(branch2) == 'Test Branch'), "description for branch2 should be 'Test Branch'"
+        assert(c.getDescriptionLength(branch2) == 11), "descriptionLength should be 11 by default"
+        assert(c.getCreationFee(branch2) == expectedCreationFee), "the creation fee for branch2 wasn't the expected creation fee (10 * ONE)"
+        assert(c.getCurrency(branch2) == cashAddr), "the currency for the branch2 should be set to the cash currency address"
+        assert(c.getWallet(branch2) == cashWallet), "the wallet for branch2 should be set to the cash wallet address"
+
+    test_defaults()
+    test_currency()
+    test_setInfo()
+    print("data_api/info.se unit tests completed")
+
 if __name__ == '__main__':
     src = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'src')
     contracts = ContractLoader(src, 'controller.se', ['mutex.se', 'cash.se', 'repContract.se'])
@@ -668,6 +711,7 @@ if __name__ == '__main__':
     test_branches(contracts, state, t)
     test_consensusData(contracts, state, t)
     test_events(contracts, state, t)
+    test_info(contracts, state, t)
     # data_api/expiringEvents.se
     # data_api/fxpFunctions.se
     # data_api/info.se
