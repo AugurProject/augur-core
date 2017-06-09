e0d6ad2895ca30c7ea1dcfa279e027cb7afe6edf
Charlton Harrison <charlton@dynet.com>
2017-06-08 22:56:55 -0500

    Controller test fixes.

--- tests/controller_tests.py
+++ tests/controller_tests.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Test the functions in src/functions/controller.se
+# Uses tests/controller_test.se as a helper (to set the controller, etc.)
+
 import ethereum.tester
 import ethereum.abi
 import serpent
@@ -11,21 +14,20 @@ import time
 import random
 from binascii import hexlify
 from ethereum import tester as t
-# t.gas_limit = 4100000
 s = t.state()
-c = s.abi_contract('controller_test.se')
+c = s.abi_contract('../src/functions/controller.se')
+d = s.abi_contract('controller_test.se')
 
-
-def test_setController():
-	print("Testing setController")
+def set_controller():
+	print("Setting Controller")
 	# set the controller = 0x0 now that we have it
-	print "Setting controller to: %s" % binascii.hexlify(c.address)
-	out = c.setController(binascii.hexlify(c.address))
+	print "Setting controller to: %s" % binascii.hexlify(d.address)
+	out = d.setController(binascii.hexlify(d.address))
 	print "setController output: %s" % out
 	assert(out == 1), "setController succeeded"
-	# Test a failure
+	# Test a setController failure  (do we even want to leave this in?)
 	try:
-		raise Exception(c.setController(binascii.hexlify(c.address)))
+		raise Exception(d.setController(binascii.hexlify(c.address)))
 	except Exception as exc:
 		#print "exception caught: %s" % exc
 		assert(isinstance(exc, ethereum.tester.TransactionFailed)), "setController should fail when attempted by non-controller address"
@@ -34,16 +36,30 @@ def test_setController():
 def test_whitelisting():
 	print("Testing Whitelists")
 	address1 = long(t.a1.encode("hex"), 16)
-	address2 = long(t.a2.encode("hex"), 16)
+
+	# Check the current mode
+	out = c.getMode()
+	print "getMode output: %s" % out
+	assert(out == 45410550817938176941147246367785497464285552864458998948296910157280029179904), "getMode should be dev"    # numeric string for 'dev'
+	# setMode to something other than dev to test
+	out = c.switchModeSoOnlyEmergencyStopsAndEscapeHatchesCanBeUsed()
+	print "switchMode output: %s" % out
+	# Mode should now be 'Decentralized':
+	out = c.getMode()
+	print "getMode output: %s" % out
+	assert(out == 30936411264679932392881305702504462444513638254699919670237862177711222423552), "getMode should be Decentralized"    # numeric string for 'Decentralized'
+
 	# Test addToWhiteList
 	out = c.addToWhitelist(t.a1)
 	print "addToWhitelist output: %s" % out
+
 	# Check that address is now Whitelisted:
 	out = c.assertIsWhitelisted(t.a1)
 	assert (out == 1), "assertIsWhitelisted failed."
+
 	# Check an address that shouldn't be Whitelisted:
 	try:
-		raise Exception(c.assertIsWhitelisted(t.a2))
+		raise Exception(c.assertIsWhitelisted(t.a2, sender=t.k1))   # In dev mode will return 1 no matter what unless called by non-controller
 	except Exception as exc:
 		assert(isinstance(exc, ethereum.tester.TransactionFailed)), "assertIsWhitelisted should fail when address is not Whitelisted"
 
@@ -56,24 +72,24 @@ def test_suicideFunds():
 	print("Testing suicideFunds")
 	# Test with non-controller addresses
 	try:
-		raise Exception(c.suicideFunds(t.a1, sender=t.k1))
+		raise Exception(d.suicideFunds(t.a1, sender=t.k1))
 	except Exception as exc:
 		assert(isinstance(exc, ethereum.tester.TransactionFailed)), "suicideFunds should fail when attempted by non-controller address"
-	# Test with another non-controller address  (redundant?)
+	# Test with another non-controller address
 	try:
-		raise Exception(c.suicideFunds(t.a2, sender=t.k2))
+		raise Exception(d.suicideFunds(t.a2, sender=t.k2))
 	except Exception as exc:
 		assert(isinstance(exc, ethereum.tester.TransactionFailed)), "suicideFunds should fail when attempted by non-controller address"
-	# Test with controller address
-	out = c.suicideFunds(t.a0, sender=t.k0)
-	assert(out == None), "suicideFunds should succeed when called by controller address"
+	# Test with controller address  (take this out if not possible)
+	#out = d.suicideFunds(d.address, sender=d.key)   # ???
+	#print "controller suicideFunds output: %s" % out
+	#assert(out == None), "suicideFunds should succeed when called by controller address"
 
 
 def test_Ownership():
 	print("Testing ownership functions")
 	address0 = long(t.a0.encode("hex"), 16)
 	address1 = long(t.a1.encode("hex"), 16)
-	address2 = long(t.a2.encode("hex"), 16)
 	#print "self.owner: %s" % self.owner
 	out = c.getOwner()
 	print "getOwner output: %s" % out
@@ -81,7 +97,7 @@ def test_Ownership():
 
 	# Test transferOwnership
 	#transferOwnership(key, newOwner, ownerBranch, proposalIndex)
-	out = c.transferOwnership(t.k1, t.a1, 0, 0)
+	out = c.transferOwnership(t.k1, t.a1, 0)
 	print "transferOwnership output: %s" % out
 	assert(out == 1), "transferOwnsership did not succeed"
 
@@ -92,7 +108,7 @@ def test_Ownership():
 
 	# transferOwnership back to original msg.sender
 	# sender must be t.k1 now or this call won't work
-	out = c.transferOwnership(t.k0, t.a0, 0, 0, sender=t.k1)
+	out = c.transferOwnership(t.k0, t.a0, 0, sender=t.k1)
 	print "transferOwnership output: %s" % out
 
 	out = c.getOwner()
@@ -105,13 +121,6 @@ def test_Ownership():
 	out = c.updateController(c.address, t.a0)
 	print "updateController output: %s" % out
 	assert(out == 1), "updateController did not succeed"
-
-
-def test_getMode():
-	print("Testing getMode")   # combine this section into one of the others?
-	out = c.getMode()
-	print "getMode output: %s" % out
-	assert(out == 0), "getMode should be dev"
 
 
 def test_lookup():
@@ -140,10 +149,9 @@ def test_lookup():
 
 # Call tests
 if __name__ == "__main__":
-	test_setController()
+	set_controller()
 	test_whitelisting()
 	test_Ownership()
-	test_getMode()
 	test_lookup()
 	test_suicideFunds()
 
