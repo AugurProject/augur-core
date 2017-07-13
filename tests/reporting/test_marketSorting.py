8707b155da0d04eace9a32b33848607072d80de2
Alex <achap5dk@gmail.com>
2017-07-12 18:29:14 -0700

    Move sorting of markets to branches. Also add attempt at optimizing internal re-adds of items in sortedLinkedLists

--- tests/reporting/test_marketSorting.py
+++ tests/reporting/test_marketSorting.py
@@ -12,38 +12,36 @@ def test_market_creation(contractsFixture):
     orders = contractsFixture.contracts['orders']
 
     market1 = contractsFixture.binaryMarket
-    scalarMarket = contractsFixture.scalarMarket
-    categoricalMarket = contractsFixture.categoricalMarket
-    
-    reportingWindow = contractsFixture.applySignature('reportingWindow', market1.getReportingWindow())
     market2 = contractsFixture.createReasonableBinaryMarket(branch, cash)
+    scalarMarket = contractsFixture.scalarMarket
+    categoricalMarket = contractsFixture.categoricalMarket    
 
-    assert reportingWindow.getNumMarkets() == 4
+    assert branch.getNumMarkets() == 4
 
-    AssertOrder(market1, categoricalMarket, scalarMarket, market2, reportingWindow=reportingWindow)
+    AssertOrder(market1, categoricalMarket, scalarMarket, market2, branch=branch)
 
     cash.publicDepositEther(value = fix('10000'), sender = tester.k1)
     cash.approve(completeSets.address, fix('10000'), sender=tester.k1)
 
     # We buy 10 complete sets in the new binary market, causing it to move to the top of the list
     assert completeSets.publicBuyCompleteSets(market2.address, fix('10'), sender=tester.k1)
-    AssertOrder(market2, market1, categoricalMarket, scalarMarket, reportingWindow=reportingWindow)
+    AssertOrder(market2, market1, categoricalMarket, scalarMarket, branch=branch)
 
     # Buying 5 complete sets of the categorical market will move it ahead of market1 into second
     assert completeSets.publicBuyCompleteSets(categoricalMarket.address, fix('5'), sender=tester.k1)
-    AssertOrder(market2, categoricalMarket, market1, scalarMarket, reportingWindow=reportingWindow)
+    AssertOrder(market2, categoricalMarket, market1, scalarMarket, branch=branch)
 
     # Buying 3 complete sets of the first binary market will not change its order
     assert completeSets.publicBuyCompleteSets(market1.address, fix('3'), sender=tester.k1)
-    AssertOrder(market2, categoricalMarket, market1, scalarMarket, reportingWindow=reportingWindow)
+    AssertOrder(market2, categoricalMarket, market1, scalarMarket, branch=branch)
 
     # Selling 8 complete sets of the new binary market will move it into third place
     assert completeSets.publicSellCompleteSets(market2.address, fix('8'), sender=tester.k1)
-    AssertOrder(categoricalMarket, market1, market2, scalarMarket, reportingWindow=reportingWindow)
+    AssertOrder(categoricalMarket, market1, market2, scalarMarket, branch=branch)
 
 def AssertOrder(*args, **kwargs):
-    reportingWindow = kwargs["reportingWindow"]
-    curMarket = reportingWindow.getMarketsHead()
+    branch = kwargs["branch"]
+    curMarket = branch.getMarketsHead()
     for market in args:
         assert curMarket == market.address
-        curMarket = reportingWindow.tryGetPrevMarket(curMarket)
+        curMarket = branch.tryGetPrevMarket(curMarket)
