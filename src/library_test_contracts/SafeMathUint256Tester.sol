8da24466561475c0a997c706440b030a490cefba
adrake33 <aarondrake@gmail.com>
2017-08-04 17:47:03 -0700

    More changes for math library test scripts

--- src/library_test_contracts/SafeMathUint256Tester.sol
+++ src/library_test_contracts/SafeMathUint256Tester.sol
@@ -4,32 +4,30 @@ import 'ROOT/libraries/math/SafeMathUint256.sol';
 
 
 contract SafeMathUint256Tester {
-    using SafeMathUint256 for uint256;
-
     function initialize() public constant {
     }
 
     function mul(uint256 _a, uint256 _b) internal constant returns (uint256) {
-        return _a.mul(_b);
+        return SafeMathUint256.mul(_a, _b);
     }
 
     function div(uint256 _a, uint256 _b) internal constant returns (uint256) {
-        return _a.div(_b);
+        return SafeMathUint256.div(_a, _b);
     }
 
     function sub(uint256 _a, uint256 _b) internal constant returns (uint256) {
-        return _a.sub(_b);
+        return SafeMathUint256.sub(_a, _b);
     }
 
     function add(uint256 _a, uint256 _b) public constant returns (uint256) {
-        return _a.add(_b);
+        return SafeMathUint256.add(_a, _b);
     }
 
-    function min() {
-        
+    function min(uint256 _a, uint256 _b) public constant returns (uint256) {
+        return SafeMathUint256.min(_a, _b);
     }
 
-    function max() {
-        
+    function max(uint256 _a, uint256 _b) public constant returns (uint256) {
+        return SafeMathUint256.max(_a, _b);
     }
 }
