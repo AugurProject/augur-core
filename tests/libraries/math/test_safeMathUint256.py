31feb1e67841df1969651be0ceabdd9abe076d0a
adrake33 <aarondrake@gmail.com>
2017-08-07 11:46:59 -0700

    Adding test cases for uint256

--- tests/libraries/math/test_safeMathUint256.py
+++ tests/libraries/math/test_safeMathUint256.py
@@ -4,44 +4,79 @@ from ethereum.tools import tester
 from ethereum.tools.tester import TransactionFailed
 from pytest import fixture, mark, lazy_fixture, raises
 
-@mark.parametrize('a, b, c', [
+@mark.parametrize('a, b, expectedResult', [
+    ((2**(256) - 1), (2**(256) - 1), "TransactionFailed"),
+    (1, 0, 0),
+    (1, 1, 1),
+    (1, 2, 2)
+])
+def test_mul(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    if (expectedResult == "TransactionFailed"):
+        with raises(TransactionFailed):
+            SafeMathInt256Tester.mul(a, b)
+    else: 
+        assert SafeMathInt256Tester.mul(a, b) == expectedResult
+
+@mark.parametrize('a, b, expectedResult', [
+    ((2**(256) - 1), (2**(256) - 1), 1),
+    (1, 0, "TransactionFailed"),
     (1, 1, 1)
 ])
-def test_mul(a, b, c, contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.mul(a, b) == c
-    print SafeMathUint256Tester.mul(a, b)
+def test_div(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    if (expectedResult == "TransactionFailed"):
+        with raises(TransactionFailed):
+            SafeMathInt256Tester.div(a, b)
+    else: 
+        assert SafeMathInt256Tester.div(a, b) == expectedResult
 
-@mark.parametrize('a, b, c', [
-    (1, 1, 1)
-])
-def test_div(a, b, c, contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.div(a, b) == c
-    print SafeMathUint256Tester.div(a, b)
-
-@mark.parametrize('a, b, c', [
+@mark.parametrize('a, b, expectedResult', [
+    ((2**(256) - 1), (2**(256) - 1), 0),
+    (1, 0, 1),
     (1, 1, 0)
 ])
-def test_sub(a, b, c, contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.sub(a, b) == c
-    print SafeMathUint256Tester.sub(a, b)
+def test_sub(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    if (expectedResult == "TransactionFailed"):
+        with raises(TransactionFailed):
+            SafeMathInt256Tester.sub(a, b)
+    else: 
+        assert SafeMathInt256Tester.sub(a, b) == expectedResult
 
-@mark.parametrize('a, b, c', [
+@mark.parametrize('a, b, expectedResult', [
+    ((2**(256) - 1), (2**(256) - 1), "TransactionFailed"),
+    (1, 0, 1),
     (1, 1, 2)
 ])
-def test_add(a, b, c, contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.add(a, b) == c
-    print SafeMathUint256Tester.add(a, b)
+def test_add(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    if (expectedResult == "TransactionFailed"):
+        with raises(TransactionFailed):
+            SafeMathInt256Tester.add(a, b)
+    else: 
+        assert SafeMathInt256Tester.add(a, b) == expectedResult
 
-def test_getUint256Min(contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.getUint256Min() == -2**(255)
-    print SafeMathUint256Tester.getUint256Min()
+@mark.parametrize('a, b, expectedResult', [
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1),
+    (1, 2, 1),
+    (2, 1, 1),
+])
+def test_min(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    assert SafeMathInt256Tester.min(a, b) == expectedResult
 
-def test_getUint256Max(contractsFixture):
-    SafeMathUint256Tester = contractsFixture.contracts['SafeMathUint256Tester']
-    assert SafeMathUint256Tester.getUint256Max() == (2**(255) - 1)
-    print SafeMathUint256Tester.getUint256Max()
+@mark.parametrize('a, b, expectedResult', [
+    (0, 0, 0),
+    (0, 1, 1),
+    (1, 0, 1),
+    (1, 1, 1),
+    (1, 2, 2),
+    (2, 1, 2),
+])
+def test_max(a, b, expectedResult, contractsFixture):
+    SafeMathInt256Tester = contractsFixture.contracts['SafeMathUint256Tester']
+    assert SafeMathInt256Tester.max(a, b) == expectedResult
