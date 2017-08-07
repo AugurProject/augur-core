f86a17303d23378449b4a5c6eecea5f24e63337f
Jack Peterson <jack@tinybike.net>
2017-08-06 23:26:12 -0700

    Added order ID to OrderBook array

--- src/extensions/OrderBook.sol
+++ src/extensions/OrderBook.sol
@@ -20,16 +20,17 @@ contract OrderBook is Controlled {
     function getOrderBook(uint256 _type, IMarket _market, uint256 _outcome, uint256 _startingOrderId, uint256 _numOrdersToLoad) constant public returns (uint256[] _orderBook) {
         OrdersFetcher _ordersFetcher = OrdersFetcher(controller.lookup("ordersFetcher"));
         uint256[100] memory _orders = _ordersFetcher.getOrderIDs(_type, _market, _outcome, _startingOrderId, _numOrdersToLoad);
-        _orderBook = new uint256[](8 * _numOrdersToLoad);
+        _orderBook = new uint256[](9 * _numOrdersToLoad);
         uint8 _numAvailableOrders = 0;
         uint8 _i = 0;
         while (_i < _numOrdersToLoad) {
             // FIXME: Once ordersFetcher.se is converted to solidity make the return value here an 8-tuple and unpack it
             uint256[8] memory _orderInfo = _ordersFetcher.getOrder(_orders[_i], _type, _market, _outcome);
             if (_orderInfo[0] != 0) {
+                _orderBook[9 * _numAvailableOrders] = _orders[_i];
                 uint8 _j = 0;
                 while (_j < 8) {
-                    _orderBook[8 * _numAvailableOrders + _j] = _orderInfo[_j];
+                    _orderBook[9 * _numAvailableOrders + _j] = _orderInfo[_j];
                     _j++;
                 }
                 _numAvailableOrders++;
