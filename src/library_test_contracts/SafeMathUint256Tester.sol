1ae9b734185f1fef2668fd5e692893b2e5cd7b6a
adrake33 <aarondrake@gmail.com>
2017-08-07 10:33:13 -0700

    Making tester functions public

--- src/library_test_contracts/SafeMathUint256Tester.sol
+++ src/library_test_contracts/SafeMathUint256Tester.sol
@@ -7,15 +7,15 @@ contract SafeMathUint256Tester {
     function initialize() public constant {
     }
 
-    function mul(uint256 _a, uint256 _b) internal constant returns (uint256) {
+    function mul(uint256 _a, uint256 _b) public constant returns (uint256) {
         return SafeMathUint256.mul(_a, _b);
     }
 
-    function div(uint256 _a, uint256 _b) internal constant returns (uint256) {
+    function div(uint256 _a, uint256 _b) public constant returns (uint256) {
         return SafeMathUint256.div(_a, _b);
     }
 
-    function sub(uint256 _a, uint256 _b) internal constant returns (uint256) {
+    function sub(uint256 _a, uint256 _b) public constant returns (uint256) {
         return SafeMathUint256.sub(_a, _b);
     }
 
