de33f5a47b957292955044c4a5bc263007e7dc63
adrake33 <aarondrake@gmail.com>
2017-08-23 15:41:43 -0700

    Minor change to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -222,7 +222,7 @@ class ContractsFixture:
                 if name == 'controller': continue
                 # remove this and Interfaces.sol when https://github.com/ethereum/solidity/issues/2665 is fixed
                 if name == 'Interfaces': continue
-                contractsToDelegate = ['orders', 'tradingEscapeHatch']
+                contractsToDelegate = ['orders','tradingEscapeHatch']
                 if name in contractsToDelegate:
                     delegationTargetName = "".join([name, "Target"])
                     self.uploadAndAddToController(path.join(directory, filename), delegationTargetName, name)
