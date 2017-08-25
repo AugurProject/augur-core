8e778a254f15e4294e2394ee2a0dbeb20522939f
adrake33 <aarondrake@gmail.com>
2017-08-25 14:56:41 -0700

    Updating function names

--- tests/trading/test_orders_fuzzy.py
+++ tests/trading/test_orders_fuzzy.py
@@ -109,7 +109,7 @@ def test_randomSorting(orderType, numOrders, withBoundingOrders, deadOrderProbab
         output = orders.saveOrder(orderID, orderType, market.address, 1, fxpPrices[i], tester.a1, outcomeID, 0, 0, betterOrderID, worseOrderID, 0, 20000000)
         assert(output == 1), "Insert order into list"
     assert(bestOrderID == int(orders.getBestOrderID(orderType, market.address, outcomeID), 16)), "Verify best order ID"
-    assert(worstOrderID == int(orders.getWorstOrderID(orderType, market.address, outcomeID), 16)), "Verify worst order ID"
+    assert(worstOrderID == int(orders.getWorstOrderId(orderType, market.address, outcomeID), 16)), "Verify worst order ID"
     for orderID in orderIDs:
         order = ordersFetcher.getOrder(orderID, orderType, market.address, outcomeID)
         orderPrice = order[DISPLAY_PRICE]
