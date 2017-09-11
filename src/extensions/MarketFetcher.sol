22985cddc76827c816d6c61113e5ebf440ac96d2
Alex <achap5dk@gmail.com>
2017-09-11 12:11:28 -0700

    Remove stub file with outdated comment

--- src/extensions/MarketFetcher.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.13;
-
-
-// TODO: Implement this using sortedList once Solidity migration is complete.
-// See: https://github.com/AugurProject/augur-core/pull/224
-
-contract MarketFetcher {
-    function stub() {}
-}
