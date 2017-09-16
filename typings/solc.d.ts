435dd98754d775459f738eb28b696ca3609bd82c
adrake33 <aarondrake@gmail.com>
2017-09-16 16:27:19 -0700

    Updating type definitions

--- typings/solc.d.ts
+++ typings/solc.d.ts
@@ -1,7 +1,17 @@
-module solc {
-    function compileStandardWrapper(input, readCallback);
-}
-
 declare module 'solc' {
-    export = solc;
+    interface CompilerInputSourceFile {
+        keccak256?: string;
+        urls: string[];
+    }
+    interface CompilerInputSourceCode {
+        keccak256?: string;
+        content: string;
+    }
+    interface CompilerInput {
+        language: "Solidity" | "serpent" | "lll" | "assembly";
+        sources: {
+            [globalName: string]: CompilerInputSourceFile,
+        };
+    }
+    function compileStandardWrapper(input: string, readCallback?: any);
 }
