0818dddb9ebc143fa539a67910fb30358b0745e5
adrake33 <aarondrake@gmail.com>
2017-09-18 13:14:57 -0700

    Removing unnecessary type definition files

--- typings/compileSolidity.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'compileSolidity' {
-    interface CompileContractsOutput {
-        output?: string;
-        error?: string[];
-    }
-}
--- typings/mkdirp.d.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-declare namespace MkDirp {
-    interface mkdirp {
-        (path: string): Promise<string[]>;
-    }
-}
-
-declare var mkdirp: MkDirp.mkdirp;
-export = mkdirp;
