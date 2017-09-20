79fab13be482cb35a360f6a392a84aa4d93b2d9b
Alex <achap5dk@gmail.com>
2017-09-19 22:01:37 -0700

    Add comment

--- src/Augur.sol
+++ src/Augur.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.13;
 import 'ROOT/Controlled.sol';
 import 'ROOT/libraries/token/StandardToken.sol';
 
-
+// AUDIT/CONSIDER: Is it better that this contract provide generic functions that are limited to whitelisted callers or for it to have many specific functions which have more limited and specific validation?
 contract Augur is Controlled {
     function trustedTransfer(StandardToken _token, address _from, address _to, uint256 _amount) public onlyWhitelistedCallers returns (bool) {
         require(_amount > 0);
