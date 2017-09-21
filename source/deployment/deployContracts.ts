469a4ded890cdabcd6f9f7dba8ac9e2087f07d49
adrake33 <aarondrake@gmail.com>
2017-09-21 15:25:51 -0700

    Updating deployment script

--- source/deployment/deployContracts.ts
+++ source/deployment/deployContracts.ts
@@ -3,7 +3,7 @@
 // import * as fs from 'fs';
 // import * as path from 'path';
 // import { SolidityContractCompiler } from "../libraries/CompileSolidity";
-// import { ContractDeployment } from "../libraries/ContractDeployment";
+// // import { ContractDeployment } from "../libraries/ContractDeployment";
 
 // try {
 //     // Compile contracts to single file
@@ -43,22 +43,26 @@ async function startTestRpc(port): Promise<boolean> {
     return true;
 }
 
-try {
+async function doWork() {
     const port = 8545;
     await startTestRpc(port);
 
     const eth = new Eth(new HttpProvider('http://localhost:' + port));
     const contract = new EthContract(eth);
 
-    eth.accounts().then((accounts) => {
-        const abi = JSON.parse('[{"constant":false,"inputs":[{"name":"_controller","type":"address"}],"name":"setController","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getTypeName","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_target","type":"address"}],"name":"suicideFunds","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]');
-        const bytecode = "60606040525b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505b5b6102a0806100566000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806392eefe9b14610054578063db0a087c146100a5578063eb03db73146100d6575b600080fd5b341561005f57600080fd5b61008b600480803573ffffffffffffffffffffffffffffffffffffffff16906020019091905050610127565b604051808215151515815260200191505060405180910390f35b34156100b057600080fd5b6100b86101cf565b60405180826000191660001916815260200191505060405180910390f35b34156100e157600080fd5b61010d600480803573ffffffffffffffffffffffffffffffffffffffff169060200190919050506101f8565b604051808215151515815260200191505060405180910390f35b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614151561018457600080fd5b816000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550600190505b5b919050565b60007f4170706c6500000000000000000000000000000000000000000000000000000090505b90565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614151561025557600080fd5b8173ffffffffffffffffffffffffffffffffffffffff16ff5b5b9190505600a165627a7a72305820b3e9080299d6aa853a0af095053d53ba5f35f3cc51f66102be8276833a5806780029";
-        const defaultTxObject = { from: accounts[0], data: bytecode};
+    const accounts = await eth.accounts();
+    const abi = JSON.parse('[{"constant":false,"inputs":[{"name":"_controller","type":"address"}],"name":"setController","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getTypeName","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_target","type":"address"}],"name":"suicideFunds","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]');
+    const bytecode = "60606040525b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505b5b6102a0806100566000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806392eefe9b14610054578063db0a087c146100a5578063eb03db73146100d6575b600080fd5b341561005f57600080fd5b61008b600480803573ffffffffffffffffffffffffffffffffffffffff16906020019091905050610127565b604051808215151515815260200191505060405180910390f35b34156100b057600080fd5b6100b86101cf565b60405180826000191660001916815260200191505060405180910390f35b34156100e157600080fd5b61010d600480803573ffffffffffffffffffffffffffffffffffffffff169060200190919050506101f8565b604051808215151515815260200191505060405180910390f35b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614151561018457600080fd5b816000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550600190505b5b919050565b60007f4170706c6500000000000000000000000000000000000000000000000000000090505b90565b60008060009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614151561025557600080fd5b8173ffffffffffffffffffffffffffffffffffffffff16ff5b5b9190505600a165627a7a72305820b3e9080299d6aa853a0af095053d53ba5f35f3cc51f66102be8276833a5806780029";
+    const defaultTxObject = { from: accounts[0], data: bytecode};
 
-        const Apple = contract(abi, bytecode);
-        const apple = await Apple.new();
-        apple.getTypeName();
-    });
-} catch (error) {
-    console.log("Error: " + error);
+    const Apple = contract(abi, bytecode, defaultTxObject);
+    const apple = await Apple.new();
+    apple.getTypeName();
 }
+
+doWork().then(() => {
+    process.exit();
+}).catch(error => {
+    console.log(error);
+    process.exit();
+});
