a5869ccb9b4e71ad49de683794fbb3ee07601ee3
Tom Haile <tom.haile@cyone.com>
2017-09-30 13:56:42 -0500

    removed extra space between tests.

--- tests/libraries/test_reentrancy_guard.py
+++ tests/libraries/test_reentrancy_guard.py
@@ -4,7 +4,6 @@ from ethereum.tools import tester
 from ethereum.tools.tester import TransactionFailed
 from pytest import fixture, mark, lazy_fixture, raises
 
-
 @fixture(scope='session')
 def testerSnapshot(sessionFixture):
     sessionFixture.resetSnapshot()
@@ -12,16 +11,13 @@ def testerSnapshot(sessionFixture):
     ReentrancyGuardHelper = sessionFixture.contracts['ReentrancyGuardHelper']
     return sessionFixture.chain.snapshot()
 
-
 @fixture
 def testerContractsFixture(sessionFixture, testerSnapshot):
     sessionFixture.chain.revert(testerSnapshot)
     return sessionFixture
 
-
 def test_nonReentrant(testerContractsFixture): 
     ReentrancyGuardHelper = testerContractsFixture.contracts['ReentrancyGuardHelper']
-
     assert ReentrancyGuardHelper.testerCanReentrant()
 
     with raises(TransactionFailed):
