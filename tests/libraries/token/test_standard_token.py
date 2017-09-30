8b15f0996fde7de63f8c0a84af50e8bb42b4cbbc
Tom Haile <tom.haile@cyone.com>
2017-09-29 19:27:03 -0500

    added more tests to standard token. tests to attempt to get negative balance. test to ping pong transfers.

--- tests/libraries/token/test_standard_token.py
+++ tests/libraries/token/test_standard_token.py
@@ -37,3 +37,85 @@ def test_eternal_approval_magic(contractsFixture):
     assert standardToken.balanceOf(tester.a0) == 4
     assert standardToken.balanceOf(tester.a1) == 96
     
+def test_create_negative_balance(contractsFixture):
+    standardToken = contractsFixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+    assert standardToken.totalSupply() == 0
+    # We get some tokens for tester 0
+    assert standardToken.faucet(100)
+    assert standardToken.balanceOf(tester.a0) == 100
+    # Tester 1 tries to send tokens from tester 0 to himself and fails
+    with raises(TransactionFailed):
+        standardToken.transferFrom(tester.a0, tester.a1, 101)
+
+def test_transfer_then_create_negative_balance(contractsFixture):
+    standardToken = contractsFixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+    assert standardToken.totalSupply() == 0
+    # We get some tokens for tester 0
+    assert standardToken.faucet(10)
+    assert standardToken.balanceOf(tester.a0) == 10
+    # approve and transfer 10 to other tester
+    assert standardToken.approve(tester.a1, 10)
+    assert standardToken.allowance(tester.a0, tester.a1) == 10
+    assert standardToken.transferFrom(tester.a0, tester.a1, 10, sender=tester.k1)
+    assert standardToken.balanceOf(tester.a1) == 10
+    # approve and allow transfer of more tokens than tester has
+    assert standardToken.approve(tester.a1, 11)
+    # try to transfer more than tester has  
+    with raises(TransactionFailed):
+        standardToken.transferFrom(tester.a0, tester.a1, 11)
+
+def test_approve_allow_transfer_more_than_allow(contractsFixture):
+    standardToken = contractsFixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+    assert standardToken.totalSupply() == 0
+    # We get some tokens for tester 0
+    assert standardToken.faucet(100000)
+    assert standardToken.balanceOf(tester.a0) == 100000
+    # approve and transfer more than in supply
+    assert standardToken.approve(tester.a1, 100)
+    assert standardToken.allowance(tester.a0, tester.a1) == 100
+    assert standardToken.balanceOf(tester.a1) == 0
+    with raises(TransactionFailed):
+        standardToken.transferFrom(tester.a0, tester.a1, 101, sender=tester.k1)
+
+def test_approve_allow_transfer_more_than_allow(contractsFixture):
+    standardToken = contractsFixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+    assert standardToken.totalSupply() == 0
+    # We get some tokens for tester 0
+    assert standardToken.faucet(100000)
+    assert standardToken.balanceOf(tester.a0) == 100000
+    # approve and transfer more than in supply
+    assert standardToken.approve(tester.a1, 100)
+    assert standardToken.allowance(tester.a0, tester.a1) == 100
+    assert standardToken.balanceOf(tester.a1) == 0
+    with raises(TransactionFailed):
+        standardToken.transferFrom(tester.a0, tester.a1, 101, sender=tester.k1)
+
+def test_ping_pong_transfers(contractsFixture):
+    standardToken = contractsFixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+    assert standardToken.totalSupply() == 0
+    # We get some tokens for tester 0
+    assert standardToken.faucet(200)
+    assert standardToken.balanceOf(tester.a0) == 200
+    assert standardToken.approve(tester.a1, 100)
+    assert standardToken.allowance(tester.a0, tester.a1) == 100
+    assert standardToken.transferFrom(tester.a0, tester.a1, 100, sender=tester.k1)
+    assert standardToken.balanceOf(tester.a0) == 100
+    assert standardToken.balanceOf(tester.a1) == 100
+    assert standardToken.allowance(tester.a0, tester.a1) == 0
+    # transfer back and forth
+    for x in range(0, 10):
+        assert standardToken.balanceOf(tester.a0) == 100
+        assert standardToken.balanceOf(tester.a1) == 100
+        assert standardToken.approve(tester.a1, 10)
+        assert standardToken.allowance(tester.a0, tester.a1) == 10
+        assert standardToken.transferFrom(tester.a0, tester.a1, 10, sender=tester.k1)
+        assert standardToken.balanceOf(tester.a0) == 90
+        assert standardToken.balanceOf(tester.a1) == 110        
+        assert standardToken.approve(tester.a0, 10, sender=tester.k1)
+        assert standardToken.allowance(tester.a1, tester.a0) == 10
+        assert standardToken.transferFrom(tester.a1, tester.a0, 10, sender=tester.k0)
+        assert standardToken.balanceOf(tester.a0) == 100
+        assert standardToken.balanceOf(tester.a1) == 100
+
+    assert standardToken.balanceOf(tester.a0) == 100
+    assert standardToken.balanceOf(tester.a1) == 100
