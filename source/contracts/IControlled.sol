a73d1f6cb24f994d2893a26a7cc0cb88783ee738
Joseph Krug <joeykrug@gmail.com>
2017-10-01 11:32:40 -0700

    Update IControlled.sol

--- source/contracts/IControlled.sol
+++ source/contracts/IControlled.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.13;
 import 'IController.sol';
 import 'reporting/IUniverse.sol';
 
+
 contract IControlled {
     function setController(IController _controller) public returns(bool);
     function suicideFunds(address _target, IUniverse _universe) public returns(bool);
