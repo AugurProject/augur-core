7b2dbe5c787f67e28ae51bd8e2369dd1bb779f1a
Joseph Krug <joeykrug@gmail.com>
2017-10-01 11:19:06 -0700

    Update ControllerUser.sol

--- tests/solidity_test_helpers/ControllerUser.sol
+++ tests/solidity_test_helpers/ControllerUser.sol
@@ -1,19 +1,22 @@
 pragma solidity ^0.4.13;
 
 import 'ROOT/IControlled.sol';
+import 'ROOT/reporting/IUniverse.sol';
 
 
 contract ControllerUser is IControlled {
     address public suicideFundsDestination;
     address public updatedController;
+    address public universe;
 
     function setController(IController _controller) public returns(bool) {
         updatedController = _controller;
         return true;
     }
 
-    function suicideFunds(address _destination) public returns (bool) {
+    function suicideFunds(address _destination, IUniverse _universe) public returns (bool) {
         suicideFundsDestination = _destination;
+        universe = _universe;
         return true;
     }
 }
