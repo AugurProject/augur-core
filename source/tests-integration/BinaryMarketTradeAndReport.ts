547f30b72a9eb0505f4c0bd17eb73496567c7b56
adrake33 <aarondrake@gmail.com>
2017-10-05 12:56:10 -0700

    Updating unit & integration tests

--- source/tests-integration/BinaryMarketTradeAndReport.ts
+++ source/tests-integration/BinaryMarketTradeAndReport.ts
@@ -16,14 +16,7 @@ describe("BinaryMarketTradeAndReport", () => {
         const testAccounts = contractDeployer.getTestAccounts();
         const controller = contractDeployer.getController();
         const contracts = await contractDeployer.getContracts();
-
-        const contractTypeNameHex = (await contracts["Universe"].getTypeName())[0];
-        const contractTypeName = binascii.unhexlify(contractTypeNameHex).replace(/\u0000/g, "");
-console.log("Contract type name: " + contractTypeName);
-
         const universe = contractDeployer.getUniverse();
-        const universeTypeName = await universe.getTypeName();
-console.log("Universe type name: " + universeTypeName);
 
         // const reportingTokenNo = contractDeployer.getReportingToken(market, [10**18,0]);
         // const reportingTokenYes = contractDeployer.getReportingToken(market, [0,10**18]);
--- source/tests-unit/Universe.ts
+++ source/tests-unit/Universe.ts
@@ -1,18 +1,22 @@
 import * as binascii from "binascii";
 import { expect } from "chai";
-import { ContractBlockchainData } from "contract-deployment";
 import { compileAndDeployContracts } from "../deployment/deployContracts";
+import { ContractDeployer } from "../libraries/ContractDeployer";
 
 
 describe("Universe", () => {
-    let contracts: ContractBlockchainData[] = [];
+    var contractDeployer: ContractDeployer;
     beforeEach(async () => {
-        const contractDeployer = await compileAndDeployContracts();
-        contracts = contractDeployer.getContracts();
+        contractDeployer = await compileAndDeployContracts();
     });
     it("#getTypeName()", async () => {
+        const contracts = contractDeployer.getContracts();
         const contractTypeNameHex = (await contracts["Universe"].getTypeName())[0];
         const contractTypeName = binascii.unhexlify(contractTypeNameHex).replace(/\u0000/g, "");
         expect(contractTypeName).to.equal("Universe");
+
+        const universe = contractDeployer.getUniverse();
+        const universeTypeName = await universe.getTypeName();
+        expect(universeTypeName).to.equal("Universe");
     });
 });
