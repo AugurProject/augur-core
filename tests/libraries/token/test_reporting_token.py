15291c9b38d004162b80167a738da30c36476bad
Tom Haile <tom.haile@cyone.com>
2017-10-14 21:03:45 -0500

    re-organized test to use fixtures. Added providing the market creator in conftests helper methods.

--- tests/libraries/token/test_reporting_token.py
+++ tests/libraries/token/test_reporting_token.py
@@ -2,12 +2,12 @@ from ethereum.tools import tester
 from datetime import timedelta
 from utils import longToHexString, stringToBytes
 from pytest import fixture, raises
-from ethereum.tools.tester import TransactionFailed
+from ethereum.tools.tester import ABIContract, TransactionFailed
 from reporting_utils import proceedToDesignatedReporting, proceedToRound1Reporting, proceedToRound2Reporting, proceedToForking, finalizeForkingMarket, initializeReportingFixture
 
 
-def test_reporting_token_creation_binary(tokenFixture):
-    bin_market = tokenFixture.binaryMarket
+def test_reporting_token_creation_binary(tokenFixture, binaryMarket):
+    bin_market = binaryMarket
     numTicks = 10 ** 18
     reportingToken_bin = tokenFixture.upload('../source/contracts/reporting/ReportingToken.sol', 'reportingToken')
     # reporting token needs same payout numerators as market
@@ -28,9 +28,9 @@ def test_reporting_token_creation_binary(tokenFixture):
     with raises(TransactionFailed, message="Reporting Token has only 3 payoutNumerators"):
         reportingToken_bin.getPayoutNumerator(2)
 
-def test_reporting_token_creation_category(tokenFixture):
+def test_reporting_token_creation_category(tokenFixture, categoricalMarket):
     reportingToken_cat = tokenFixture.upload('../source/contracts/reporting/ReportingToken.sol', 'reportingToken')
-    cat_market = tokenFixture.categoricalMarket
+    cat_market = categoricalMarket
     numTicks = 3 * 10 ** 17
     # reporting token needs same payout numerators as market
     with raises(TransactionFailed, message="payout numerators can not be more than market"):
@@ -44,14 +44,14 @@ def test_reporting_token_creation_category(tokenFixture):
     with raises(TransactionFailed, message="Reporting Token has only 3 payoutNumerators"):
         reportingToken_cat.getPayoutNumerator(3)
 
-def test_reporting_token_getters(tokenFixture):
+def test_reporting_token_getters(tokenFixture, categoricalMarket, universe):
     reportingToken = tokenFixture.upload('../source/contracts/reporting/ReportingToken.sol', 'reportingToken')
-    market = tokenFixture.categoricalMarket
+    market = categoricalMarket
 
     numTicks = 3 * 10 ** 17
     assert reportingToken.initialize(market.address, [0, 1, numTicks - 1], False)        
     assert reportingToken.getTypeName() == stringToBytes("ReportingToken")
-    assert reportingToken.getUniverse() == tokenFixture.universe.address
+    assert reportingToken.getUniverse() == universe.address
     assert reportingToken.getMarket() == market.address
     assert reportingToken.getPayoutDistributionHash() == market.derivePayoutDistributionHash([0, 1, numTicks - 1], False)
     assert reportingToken.getReportingWindow() == market.getReportingWindow()
@@ -59,9 +59,9 @@ def test_reporting_token_getters(tokenFixture):
     assert reportingToken.getReputationToken() == reportingWindow.getReputationToken()
     assert reportingToken.isValid()
 
-def test_reporting_token_valid_checks(tokenFixture):
+def test_reporting_token_valid_checks(tokenFixture, universe, cash):
     reportingToken = tokenFixture.upload('../source/contracts/reporting/ReportingToken.sol', 'reportingToken')
-    market = tokenFixture.createReasonableCategoricalMarket(tokenFixture.universe, 5, tokenFixture.cash)
+    market = tokenFixture.createReasonableCategoricalMarket(universe, 5, cash)
     numTicks = 3 * 10 ** 17
     assert reportingToken.initialize(market.address, [numTicks/5, numTicks/5, numTicks/5, numTicks/5, numTicks/5], False)
     assert reportingToken.isValid()
@@ -69,10 +69,10 @@ def test_reporting_token_valid_checks(tokenFixture):
     with raises(TransactionFailed, message="Reporting Token can not be initialized invalid"):
         reportingToken.initialize(market.address, [numTicks/5, numTicks/5, numTicks/5, numTicks/5, numTicks/5], True)
 
-def test_reporting_token_buy_no_report_state(tokenFixture):
+def test_reporting_token_buy_no_report_state(tokenFixture, universe, cash):
     numTicks = 3 * 10 ** 17
     testerAddress = tester.a0
-    market = tokenFixture.createReasonableCategoricalMarket(tokenFixture.universe, 3, tokenFixture.cash)
+    market = tokenFixture.createReasonableCategoricalMarket(universe, 3, cash)
     reportingToken = tokenFixture.upload('../source/contracts/reporting/ReportingToken.sol', 'reportingToken')
     reportingWindow = tokenFixture.applySignature('ReportingWindow', market.getReportingWindow())
 
@@ -82,35 +82,35 @@ def test_reporting_token_buy_no_report_state(tokenFixture):
         reportingToken.buy(1) 
 
     tokenFixture.chain.head_state.timestamp = reportingWindow.getDisputeStartTime() + 1
-    assert market.getReportingState() == tokenFixture.constants.AWAITING_NO_REPORT_MIGRATION()
+    assert market.getReportingState() == tokenFixture.contracts['Constants'].AWAITING_NO_REPORT_MIGRATION()
 
     reportingToken = tokenFixture.applySignature('ReportingToken', market.getReportingToken([0, 0, numTicks]))
-    designatedReporterStake = tokenFixture.universe.getDesignatedReportStake()
+    designatedReporterStake = universe.getDesignatedReportStake()
     originalTesterBalance = reportingToken.balanceOf(testerAddress)
     newTesterBalance = originalTesterBalance + 1 + designatedReporterStake
     assert reportingToken.buy(1)
     assert reportingToken.balanceOf(testerAddress) == newTesterBalance
     
-def test_reporting_token_buy_get_more(tokenFixture):
+def test_reporting_token_buy_get_more(tokenFixture, universe, cash):
     numTicks = 3 * 10 ** 17
     testerAddress = tester.a0
-    market = tokenFixture.createReasonableCategoricalMarket(tokenFixture.universe, 3, tokenFixture.cash)
+    market = tokenFixture.createReasonableCategoricalMarket(universe, 3, cash)
     reportingWindow = tokenFixture.applySignature('ReportingWindow', market.getReportingWindow())
     reportingToken = tokenFixture.applySignature('ReportingToken', market.getReportingToken([0, 0, numTicks]))
     tokenFixture.chain.head_state.timestamp = reportingWindow.getDisputeStartTime() + 1
-    assert market.getReportingState() == tokenFixture.constants.AWAITING_NO_REPORT_MIGRATION()
+    assert market.getReportingState() == tokenFixture.contracts['Constants'].AWAITING_NO_REPORT_MIGRATION()
     reportingToken.buy(1)
-    assert market.getReportingState() == tokenFixture.constants.ROUND1_REPORTING()
+    assert market.getReportingState() == tokenFixture.contracts['Constants'].ROUND1_REPORTING()
 
     #with raises(TransactionFailed, message="Can not buy when market is in AWAITING_NO_REPORT_MIGRATION"):
     #    reportingToken.buy(1)
 
 
-def test_reporting_token_buy_designated_reporter_state(tokenFixture):    
-    market = tokenFixture.categoricalMarket
+def test_reporting_token_buy_designated_reporter_state(tokenFixture, categoricalMarket, universe):    
+    market = categoricalMarket
     testerAddress = tester.a0
     otherAddress = tester.a1
-    reputationToken = tokenFixture.applySignature('ReputationToken', tokenFixture.universe.getReputationToken())
+    reputationToken = tokenFixture.applySignature('ReputationToken', universe.getReputationToken())
     numTicks = 3 * 10 ** 17
     reportingWindow = tokenFixture.applySignature('ReportingWindow', market.getReportingWindow())
     reportingToken = tokenFixture.applySignature('ReportingToken', market.getReportingToken([numTicks/3, numTicks/3, numTicks/3]))
@@ -119,8 +119,8 @@ def test_reporting_token_buy_designated_reporter_state(tokenFixture):
         reportingToken.buy(2, sender=tester.k1)
 
     # Proceed to the DESIGNATED REPORTING phase
-    proceedToDesignatedReporting(tokenFixture, market, [0, 0, numTicks])
-    assert market.getReportingState() == tokenFixture.constants.DESIGNATED_REPORTING()
+    proceedToDesignatedReporting(tokenFixture, universe, market, [0, 0, numTicks])
+    assert market.getReportingState() == tokenFixture.contracts['Constants'].DESIGNATED_REPORTING()
 
     # Not designated reporter throws error
     with raises(TransactionFailed, message="Only designated reporter can buy a token in DESIGNATED_REPORTING"):
@@ -130,7 +130,7 @@ def test_reporting_token_buy_designated_reporter_state(tokenFixture):
     with raises(TransactionFailed, message="Designated reporter needs to buy same as bond amount in DESIGNATED_REPORTING"):
         reportingToken.buy(1)
 
-    designatedReporterStake = tokenFixture.universe.getDesignatedReportStake()
+    designatedReporterStake = universe.getDesignatedReportStake()
     # need to buy the correct bond amount
     with raises(TransactionFailed, message="Designated reporter needs to buy same as bond amount in DESIGNATED_REPORTING"):
         reportingToken.buy(designatedReporterStake + 1)
@@ -140,12 +140,11 @@ def test_reporting_token_buy_designated_reporter_state(tokenFixture):
     assert reportingToken.buy(designatedReporterStake)
     assert reportingToken.balanceOf(testerAddress) == newTesterBalance
 
-def test_reporting_token_redeem(tokenFixture):
+def test_reporting_token_redeem(tokenFixture, universe, cash):
     numTicks = 10 ** 18
-    denominationToken = tokenFixture.cash
+    denominationToken = cash
     feePerEthInWei = 10**10
     endTime = long(tokenFixture.chain.head_state.timestamp + timedelta(days=1).total_seconds())
-    universe = tokenFixture.universe
 
     market_other = tokenFixture.createBinaryMarket(universe, endTime, feePerEthInWei, denominationToken, tester.a8, numTicks, tester.k9)
     market_forking = tokenFixture.createBinaryMarket(universe, endTime, feePerEthInWei, denominationToken, tester.a1, numTicks)
@@ -156,28 +155,37 @@ def test_reporting_token_redeem(tokenFixture):
     original_market_creator_other = reportingToken_other.balanceOf(tester.a9) 
 
     # Designated Report votes that Market is invalid
-    proceedToDesignatedReporting(tokenFixture, market_other, [0, numTicks])
-    assert market_other.getReportingState() == tokenFixture.constants.DESIGNATED_REPORTING()
+    proceedToDesignatedReporting(tokenFixture, universe, market_other, [0, numTicks])
+    assert market_other.getReportingState() == tokenFixture.contracts['Constants'].DESIGNATED_REPORTING()
     reportingToken_other.buy(designatedReporterStake, sender=tester.k8)
     assert reportingToken_other.balanceOf(tester.a8) == designatedReporterStake
     assert market_other.isContainerForReportingToken(reportingToken_other.address)
 
     # kick this market into the DESIGNATED_REPORTING phase
-    proceedToDesignatedReporting(tokenFixture, market_forking, [0, numTicks])
+    proceedToDesignatedReporting(tokenFixture, universe, market_forking, [0, numTicks])
 
-    proceedToRound1Reporting(tokenFixture, market_forking, False, tester.a6, [0, numTicks], [numTicks, 0])
-    assert market_forking.getReportingState() == tokenFixture.constants.ROUND1_REPORTING()
+    proceedToRound1Reporting(tokenFixture, universe, market_forking, False, tester.a6, [0, numTicks], [numTicks, 0])
+    assert market_forking.getReportingState() == tokenFixture.contracts['Constants'].ROUND1_REPORTING()
 
-    proceedToRound2Reporting(tokenFixture, market_forking, False, tester.a6, tester.k3, [numTicks, 0], [0, numTicks], tester.k2, [0, numTicks], [numTicks, 0])
-    assert market_forking.getReportingState() == tokenFixture.constants.ROUND2_REPORTING()
+    proceedToRound2Reporting(tokenFixture, universe, market_forking, False, tester.a6, tester.k3, [numTicks, 0], [0, numTicks], tester.k2, [0, numTicks], [numTicks, 0])
+    assert market_forking.getReportingState() == tokenFixture.contracts['Constants'].ROUND2_REPORTING()
 
     # Proceed to the FORKING phase
-    proceedToForking(tokenFixture, market_forking, False, tester.k1, tester.k3, tester.k4, [0,numTicks], [numTicks,0], tester.k2, [numTicks,0], [numTicks,0], [0,numTicks])
-    assert market_forking.getReportingState() == tokenFixture.constants.FORKING()
+    proceedToForking(tokenFixture, universe, market_forking, True, tester.k1, tester.k3, tester.k4, [0,numTicks], [numTicks,0], tester.k2, [numTicks,0], [numTicks,0], [0,numTicks])
+    assert market_forking.getReportingState() == tokenFixture.contracts['Constants'].FORKING()
+
+    # durring a fork non-forking markets should be in limbo and allow users to redeemDisavowedTokens
+    assert market_other.getReportingState() == tokenFixture.contracts['Constants'].DESIGNATED_DISPUTE()
+    reportingToken_non_forking = tokenFixture.applySignature('ReportingToken', market_other.getReportingToken([0, numTicks]))
+    assert market_other.isContainerForReportingToken(reportingToken_non_forking.address) == True
 
     # Finalize the market
-    finalizeForkingMarket(tokenFixture, market_forking, True, tester.a1, tester.k1, tester.a0, tester.k0, tester.a2, tester.k2, [0,numTicks], [numTicks,0])
-    market_forking.getReportingState() == tokenFixture.constants.FINALIZED()
+    #finalizeForkingMarket(tokenFixture, universe, market_forking, True, tester.a1, tester.k1, tester.a0, tester.k0, tester.a2, tester.k2, [0,numTicks], [numTicks,0])
+    #market_forking.getReportingState() == tokenFixture.contracts['Constants'].FINALIZED()
+
+    # users of non-forking unresolved markets should get tokens back
+    assert reportingToken_other.redeemDisavowedTokens(tester.a9)
+    assert reportingToken_other.balanceOf(tester.a9) == original_a9
 
     # progress to next reporting window
     reportingWindow = tokenFixture.applySignature('ReportingWindow', universe.getNextReportingWindow())
@@ -191,32 +199,44 @@ def test_reporting_token_redeem(tokenFixture):
     assert reportingToken_other.redeemDisavowedTokens(tester.a9)
     assert reportingToken_other.balanceOf(tester.a8) == original_market_creator_other + designatedReporterStake
 
-    # users of non-forking unresolved markets should get tokens back
-    #assert reportingToken_other.redeemDisavowedTokens(tester.a9)
-    #assert reportingToken_other.balanceOf(tester.a9) == original_a9
-    
     # (Direction) When a market needs to be reset (reporting wise), 
     # all associated dispute bonds and tokens become "disavowed".  
     # This means that anyone who holds the token/bond can redeem it at face value.  
     # No loss, no gain.
 
-@fixture
-def sessionSnapshot(contractsFixture):
-    contractsFixture.resetSnapshot()
-    return contractsFixture.createSnapshot()
+@fixture(scope="session")
+def localSnapshot(fixture, kitchenSinkSnapshot):
+    fixture.resetToSnapshot(kitchenSinkSnapshot)
+    universe = ABIContract(fixture.chain, kitchenSinkSnapshot['universe'].translator, kitchenSinkSnapshot['universe'].address)
+    market = ABIContract(fixture.chain, kitchenSinkSnapshot['binaryMarket'].translator, kitchenSinkSnapshot['binaryMarket'].address)
+    return initializeReportingFixture(fixture, universe, market)
 
 @fixture
-def tokenFixture(contractsFixture, sessionSnapshot):
-    contractsFixture.resetToSnapshot(sessionSnapshot)
+def universe(fixture, kitchenSinkSnapshot):
+    universe = ABIContract(fixture.chain, kitchenSinkSnapshot['universe'].translator, kitchenSinkSnapshot['universe'].address)
+    return universe
+
+@fixture
+def binaryMarket(fixture, kitchenSinkSnapshot):
+    market = ABIContract(fixture.chain, kitchenSinkSnapshot['binaryMarket'].translator, kitchenSinkSnapshot['binaryMarket'].address)
+    return market
+
+@fixture
+def cash(fixture, kitchenSinkSnapshot):
+    cash = ABIContract(fixture.chain, kitchenSinkSnapshot['cash'].translator, kitchenSinkSnapshot['cash'].address)
+    return cash
+
+@fixture
+def categoricalMarket(fixture, kitchenSinkSnapshot):
+    market = ABIContract(fixture.chain, kitchenSinkSnapshot['categoricalMarket'].translator, kitchenSinkSnapshot['categoricalMarket'].address)
+    return market
+
+@fixture
+def tokenFixture(fixture, universe, localSnapshot):
+    fixture.resetToSnapshot(localSnapshot)
     value = 1 * 10**6 * 10**18
-    testerAddress = tester.a0
-    tester1Address = tester.a1
-    reputationToken = contractsFixture.applySignature('ReputationToken', contractsFixture.universe.getReputationToken())
-    legacyRepContract = contractsFixture.contracts['LegacyRepContract']
-    legacyRepContract.faucet(long(11 * 10**6 * 10**18))
-    legacyRepContract.approve(reputationToken.address, 11 * 10**6 * 10**18)
-    reputationToken.migrateFromLegacyRepContract()
+    reputationToken = fixture.applySignature('ReputationToken', universe.getReputationToken())
     # Give tester reputation tokens
-    for testAccount in [tester.a1, tester.a2, tester.a3, tester.a4, tester.a5, tester.a6, tester.a7, tester.a8, tester.a9]:
+    for testAccount in [tester.a4, tester.a5, tester.a6, tester.a7, tester.a8, tester.a9]:
         reputationToken.transfer(testAccount, value)
-    return contractsFixture
+    return fixture
