2015c065087044969bff5eaec8c52795f48dd623
adrake33 <aarondrake@gmail.com>
2017-10-16 09:50:53 -0700

    Use a known account when starting Geth instead of creating a new account

--- geth-dev-node/start.sh
+++ geth-dev-node/start.sh
@@ -1,3 +1,2 @@
-geth --password /app/password.txt account new
 geth init /app/genesis.json
-geth --password /app/password.txt --unlock 0 --mine --networkid=10000 --ws --wsapi eth,net,web3,personal --wsport 8546 --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --targetgaslimit 6700000 --ethash.dagdir "/app/.ethash"
+geth --keystore /app/keystore --password /app/password.txt --unlock 0x9cf532438ec2bd4ad1c3364df4e3a7c4bac8269c --mine --networkid=10000 --ws --wsapi eth,net,web3,personal --wsport 8546 --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --targetgaslimit 6700000 --ethash.dagdir "/app/.ethash"
