f385ce7947eac9e4cec001313b3e308c62f35846
adrake33 <aarondrake@gmail.com>
2017-10-16 23:21:14 -0700

    Making requested PR changes

--- geth-dev-node/start.sh
+++ geth-dev-node/start.sh
@@ -1,2 +1,2 @@
-geth --datadir /home/\.ethereum init /home/genesis.json
-geth --datadir /home/\.ethereum --keystore /home/\.ethereum/keystore --password /home/password.txt --unlock "0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7" --mine --ws --wsapi eth,net,web3,personal --wsport 8546 --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --targetgaslimit 6500000
+#TODO: Create TOML file rather than using command line parameters
+geth --datadir /geth --keystore /geth/keys --password /geth/password.txt --unlock "0xc5ed899b0878656feb06467e2e9ede3ae73cbcb7" --mine --ws --wsapi eth,net,web3,personal --wsport 8546 --rpc --rpcapi eth,net,web3,personal,miner --rpcaddr 0.0.0.0 --targetgaslimit 6500000
