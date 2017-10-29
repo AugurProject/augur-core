78aced3bc519771eba2d4581d889f5d0f47cac04
Tom Haile <tom.haile@cyone.com>
2017-10-29 17:07:57 -0500

    removed calling emitTransferLogs from internalTransfer because it causes 3 events instead of 1 in existing tests. Not sure why more investigation is needed.

--- source/contracts/libraries/token/BasicToken.sol
+++ source/contracts/libraries/token/BasicToken.sol
@@ -35,7 +35,7 @@ contract BasicToken is ERC20Basic {
     function internalTransfer(address _from, address _to, uint256 _value) internal returns (bool) {
         balances[_from] = balances[_from].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        emitTransferLogs(_from, _to, _value);
+        Transfer(_from, _to, _value);
         return true;
     }
 
