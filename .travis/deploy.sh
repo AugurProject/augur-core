ee0968e2b2e6bbb456855f19a6aa956353c86e52
Paul Gebheim <paul@onetaste.us>
2017-11-01 09:57:51 -0700

    Deploy scripts for running locally

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,8 @@ set -e
 echo "Deploying Augur..."
 
 export ETHEREUM_HOST="ropsten.augur.net"
-export ETHEREUM_PORT="8546"
+export ETHEREUM_PORT="8545"
 export ETHEREUM_PRIVATE_KEY=$ROPSTEN_PRIVATE_KEY
-docker container run -e ETHEREUM_HOST -e ETHERUM_PORT -e augur/core-deploy:latest
+
+#docker container run -e ETHEREUM_HOST -e ETHERUM_PORT -e ETHEREUM_PRIVATE_KEY -it augur/core-deploy:latest
+node output/deployment/deployContracts.js
