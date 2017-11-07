c9e47341a547614a8aefd9101832b29ddaa4d314
Paul Gebheim <paul@onetaste.us>
2017-11-07 00:17:15 -0800

    Add rockaway

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -18,7 +18,11 @@ case $1 in
   "kovan")
     host="kovan.augur.net"
     privateKey=$KOVAN_PRIVATE_KEY
+    ;;
+  "rockaway")
+    host="localhost"
+    privateKey=$ROCKAWAY_PRIVATE_KEY
 esac
 
 #docker container run -e ETHEREUM_HOST -e ETHERUM_PORT -e ETHEREUM_PRIVATE_KEY -it augur/core-deploy:latest
-ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node --inspect-brk output/deployment/compileAndDeploy.js
+ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node output/deployment/compileAndDeploy.js
