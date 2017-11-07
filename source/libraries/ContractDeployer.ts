c1a1e096470814a6fe8015f1e339c9bdb5e9e0d5
Paul Gebheim <paul@onetaste.us>
2017-11-07 01:19:51 -0800

    Fix logging

--- source/libraries/ContractDeployer.ts
+++ source/libraries/ContractDeployer.ts
@@ -143,10 +143,10 @@ export class ContractDeployer {
         const gasEstimate = await this.connector.ethjsQuery.estimateGas({ from: this.accountManager.defaultAddress, data: data, gas: new BN(6500000) });
         const nonce = await this.accountManager.nonces.get(this.accountManager.defaultAddress);
         const signedTransaction = await this.accountManager.signTransaction({ gas: gasEstimate, gasPrice: this.configuration.gasPrice, data: data});
-        console.log(`Upload contract: ${contractName}, txnhash: ${transactionHash}, nonce: ${nonce}, gas: ${gasEstimate}`);
         const transactionHash = await this.connector.ethjsQuery.sendRawTransaction(signedTransaction);
+        console.log(`Upload contract: ${contract.contractName}, txnhash: ${transactionHash}, nonce: ${nonce}, gas: ${gasEstimate}`);
         const receipt = await this.connector.waitForTransactionReceipt(transactionHash, failureDetails);
-        console.log(`Uploaded contract: ${contractName}: \"${receipt.contractAddress}\"`);
+        console.log(`Uploaded contract: ${contract.contractName}: \"${receipt.contractAddress}\"`);
         return receipt.contractAddress;
     }
 
