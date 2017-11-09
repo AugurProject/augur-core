852e633fa5072a057dadf14ab32f2dfeaff3f0fc
Paul Gebheim <paul@onetaste.us>
2017-11-09 02:47:57 -0800

    Update deploy script with some defaults

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,19 +11,30 @@ case $1 in
   "ropsten")
     host="ropsten.augur.net"
     privateKey=$ROPSTEN_PRIVATE_KEY
+    gasPrice=20
     ;;
   "rinkeby")
     host="rinkeby.augur.net"
     privateKey=$RINKEBY_PRIVATE_KEY
+    gasPrice=20
+    controller="0xa76ecf40e366d3462a857cbb4695158e4601a8f6"
     ;;
   "kovan")
     host="kovan.augur.net"
     privateKey=$KOVAN_PRIVATE_KEY
+    gasPrice=1
     ;;
   "rockaway")
     host="localhost"
     privateKey=$ROCKAWAY_PRIVATE_KEY
+    gasPrice=1
 esac
 
-#docker container run -e ETHEREUM_HOST -e ETHERUM_PORT -e ETHEREUM_PRIVATE_KEY -it augur/core-deploy:latest
-ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node $inspect output/deployment/compileAndDeploy.js
+docker container run -it \
+  -e AUGUR_CONTROLLER_ADDRESS=$controller \
+  -e ETHEREUM_GAS_PRICE_IN_NANOETH=$gasPrice \
+  -e ETHEREUM_HOST=$host \
+  -e ETHEREUM_PORT=$port \
+  -e ETHEREUM_PRIVATE_KEY=$privateKey \
+  augur/core-deploy:latest
+#ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node $inspect output/deployment/compileAndDeploy.js
