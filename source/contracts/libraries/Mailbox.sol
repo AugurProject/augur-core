2c7c1dd36b1512b440faea205111520f5e9a37e7
Alex <achap5dk@gmail.com>
2017-11-14 13:34:21 -0800

    Market creator fees now go to a Mailbox contract associated with the market and owned by the creator.

---
+++ source/contracts/libraries/Mailbox.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.4.17;
+
+import 'libraries/DelegationTarget.sol';
+import 'libraries/Ownable.sol';
+import 'libraries/token/ERC20Basic.sol';
+import 'libraries/IMailbox.sol';
+import 'libraries/Initializable.sol';
+import 'trading/ICash.sol';
+
+
+contract Mailbox is DelegationTarget, Ownable, Initializable, IMailbox {
+    function initialize(address _owner) public onlyInGoodTimes beforeInitialized returns (bool) {
+        endInitialization();
+        owner = _owner;
+        return true;
+    }
+
+    //As a delegation target we cannot override the fallback, so we provide a specific method to deposit ETH
+    function depositEther() public payable onlyInGoodTimes returns (bool) {
+        return true;
+    }
+
+    function withdrawEther() public onlyOwner returns (bool) {
+        // Withdraw any Cash balance
+        ICash _cash = ICash(controller.lookup("Cash"));
+        uint256 _tokenBalance = _cash.balanceOf(this);
+        if (_tokenBalance > 0) {
+            _cash.withdrawEtherTo(owner, _tokenBalance);
+        }
+        // Withdraw any ETH balance
+        if (this.balance > 0) {
+            require(owner.call.value(this.balance)());
+        }
+        return true;
+    }
+
+    function withdrawTokens(ERC20Basic _token) public onlyOwner returns (bool) {
+        uint256 _balance = _token.balanceOf(this);
+        require(_token.transfer(owner, _balance));
+        return true;
+    }
+}
---
+++ tests/libraries/test_mailbox.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+from ethereum.tools import tester
+from ethereum.tools.tester import TransactionFailed
+from pytest import fixture, raises
+from utils import stringToBytes, EtherDelta, TokenDelta
+
+def test_mailbox_eth_happy_path(localFixture, mailbox):
+    # We can send some ETH to the mailbox
+    with EtherDelta(100, mailbox.address, localFixture.chain, "Deposit did not work"):
+        assert mailbox.depositEther(value=100)
+
+    # We can also withdraw the ETH balance of the mailbox
+    with EtherDelta(100, tester.a0, localFixture.chain, "Withdraw did not work"):
+        assert mailbox.withdrawEther()
+
+def test_mailbox_tokens_happy_path(localFixture, mailbox, token):
+    # We can send some Tokens to the mailbox
+    assert token.faucet(100)
+
+    with TokenDelta(token, 100, mailbox.address, "Token deposit did not work"):
+        with TokenDelta(token, -100, tester.a0, "Token deposit did not work"):
+            token.transfer(mailbox.address, 100)
+
+    # The mailbox owner can withdraw these tokens
+    with TokenDelta(token, 100, tester.a0, "Token withdraw did not work"):
+        with TokenDelta(token, -100, mailbox.address, "Token withdraw did not work"):
+            mailbox.withdrawTokens(token.address)
+
+def test_mailbox_eth_failure(localFixture, mailbox):
+    # We send some ETH to the mailbox
+    with EtherDelta(100, mailbox.address, localFixture.chain, "Deposit did not work"):
+        assert mailbox.depositEther(value=100)
+
+    # Withdrawing as someone other than the owner will fail
+    with raises(TransactionFailed):
+        mailbox.withdrawEther(sender=tester.k1)
+
+def test_mailbox_tokens_failure(localFixture, mailbox, token):
+    # We send some Tokens to the mailbox
+    assert token.faucet(100)
+
+    with TokenDelta(token, 100, mailbox.address, "Token deposit did not work"):
+        with TokenDelta(token, -100, tester.a0, "Token deposit did not work"):
+            token.transfer(mailbox.address, 100)
+
+    # Withdrawing as someone other than the owner will fail
+    with raises(TransactionFailed):
+        mailbox.withdrawTokens(token.address, sender=tester.k1)
+
+def test_mailbox_cash_happy_path(localFixture, mailbox, cash):
+    # We can send some Cash to the mailbox
+    assert cash.depositEther(value=100)
+    assert cash.balanceOf(tester.a0) == 100
+
+    with TokenDelta(cash, 100, mailbox.address, "Deposit did not work"):
+        assert cash.transfer(mailbox.address, 100)
+
+    # We can withdraw "Ether" and the Cash balance in the mailbox will be given to the owner as Ether
+    with EtherDelta(100, tester.a0, localFixture.chain, "Withdraw did not work"):
+        assert mailbox.withdrawEther()
+
+@fixture(scope="session")
+def localSnapshot(fixture, controllerSnapshot):
+    fixture.resetToSnapshot(controllerSnapshot)
+
+    # Upload a token
+    fixture.uploadAndAddToController("solidity_test_helpers/StandardTokenHelper.sol")
+
+    # Upload Cash
+    cash = fixture.uploadAndAddToController("../source/contracts/trading/Cash.sol")
+    cash.setController(fixture.contracts['Controller'].address)
+
+    # Upload the mailbox
+    name = "Mailbox"
+    targetName = "MailboxTarget"
+    fixture.uploadAndAddToController("../source/contracts/libraries/Mailbox.sol", targetName, name)
+    fixture.uploadAndAddToController("../source/contracts/libraries/Delegator.sol", name, "delegator", constructorArgs=[fixture.contracts['Controller'].address, stringToBytes(targetName)])
+    fixture.contracts[name] = fixture.applySignature(name, fixture.contracts[name].address)
+    fixture.contracts[name].initialize(tester.a0)
+    return fixture.createSnapshot()
+
+@fixture
+def localFixture(fixture, localSnapshot):
+    fixture.resetToSnapshot(localSnapshot)
+    return fixture
+
+@fixture
+def mailbox(localFixture):
+    return localFixture.contracts['Mailbox']
+
+@fixture
+def token(localFixture):
+    return localFixture.contracts['StandardTokenHelper']
+
+@fixture
+def cash(localFixture):
+    return localFixture.contracts['Cash']
