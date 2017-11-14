6b0f71a9a84ceeef4eca16c8c3407f22dac6d112
Paul Gebheim <paul@onetaste.us>
2017-11-14 15:57:52 -0800

    Use scripts from augur-contract repo to update it

--- support/deploy/start.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/AugurProject/augur-contracts ./augur-contracts
-
-node output/deployment/deployContracts.js
-
-#bash ./augur-contracts/scripts/transform-output.sh
-npm install -g jq.node dotsunited-merge-json
-bash ./transform-output.sh output/contracts
