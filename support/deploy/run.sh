a1a4753f0dd0a0ee5e0d404defdcfa4d691910b3
Paul Gebheim <paul@onetaste.us>
2017-11-14 14:59:44 -0800

    Support deploying inside/outside docker

--- support/deploy/run.sh
+++ support/deploy/run.sh
@@ -7,7 +7,7 @@ echo "Deploying Augur to $1"
 port="8545"
 inspect="" #"--inspect--brk"
 
-case $1 in
+case $2 in
   "ropsten")
     host="ropsten.augur.net"
     privateKey=$ROPSTEN_PRIVATE_KEY
@@ -28,16 +28,31 @@ case $1 in
     host="localhost"
     privateKey=$ROCKAWAY_PRIVATE_KEY
     gasPrice=1
+    ;;
+  *)
+    echo "Must specify a network to deploy"
+    exit 1
+    ;;
 esac
 
-AUGUR_CONTROLLER_ADDRESS=$controller ETHEREUM_GAS_PRICE_IN_NANOETH=$gasPrice ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node $inspect output/deployment/compileAndDeploy.js
+case $1 in
+  "docker")
+    docker container run -it \
+      -e AUGUR_CONTROLLER_ADDRESS=$controller \
+      -e ETHEREUM_GAS_PRICE_IN_NANOETH=$gasPrice \
+      -e ETHEREUM_HOST=$host \
+      -e ETHEREUM_PORT=$port \
+      -e ETHEREUM_PRIVATE_KEY=$privateKey \
+      augur/core-deploy:latest
+    ;;
+  "direct")
+    AUGUR_CONTROLLER_ADDRESS=$controller ETHEREUM_GAS_PRICE_IN_NANOETH=$gasPrice ETHEREUM_HOST=$host ETHEREUM_PORT=$port ETHEREUM_PRIVATE_KEY=$privateKey node $inspect output/deployment/compileAndDeploy.js
+    ;;
+  *)
+    echo "Must specifiy either docker or direct as first argument"
+    exit 1
+    ;;
+esac
 
 exit
 
-docker container run -it \
-  -e AUGUR_CONTROLLER_ADDRESS=$controller \
-  -e ETHEREUM_GAS_PRICE_IN_NANOETH=$gasPrice \
-  -e ETHEREUM_HOST=$host \
-  -e ETHEREUM_PORT=$port \
-  -e ETHEREUM_PRIVATE_KEY=$privateKey \
-  augur/core-deploy:latest
