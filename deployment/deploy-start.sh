9c98af5f4922fa69f1a4065355f7cc157f54c0d2
Paul Gebheim <paul@onetaste.us>
2017-11-13 22:43:14 -0800

    Move all of the deployment support into deployment

--- deployment/deploy-start.sh
+++ deployment/deploy-start.sh
@@ -3,5 +3,5 @@ git clone https://github.com/AugurProject/augur-contracts ./augur-contracts
 node output/deployment/deployContracts.js
 
 #bash ./augur-contracts/scripts/transform-output.sh
-npm install -g jq.node
+npm install -g jq.node dotsunited-merge-json
 bash ./transform-output.sh output/contracts
