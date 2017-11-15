2f73fcc36e6b20c4c24304aeb07225a489005384
Paul Gebheim <paul@onetaste.us>
2017-11-15 13:37:26 -0800

    Fix docker commands

--- support/test/unit/run.sh
+++ support/test/unit/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Starting Solidity Linting..."
-docker run --entrypoint=npx augur/augur-core-test:latest solium --dir . --reporter=gcc
+docker run --rm -it --entrypoint=npx augur/augur-core-test:latest solium --dir . --reporter=gcc
 echo "Done Solidity Linting..."
 
 if [[ "$1" == "console" ]]; then
-  docker run -rm -it --entrypoint /bin/bash augur/augur-core-test:latest
+  docker run --rm -it --entrypoint /bin/bash augur/augur-core-test:latest
 else
-  docker run -rm -it augur/augur-core-test:latest
+  docker run --rm -it augur/augur-core-test:latest
 fi
