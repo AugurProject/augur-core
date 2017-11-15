3a9a270e58b2063e0f5b14e118bf855141bac622
Paul Gebheim <paul@onetaste.us>
2017-11-15 11:53:27 -0800

    Unit test reference new docker image name

--- support/test/unit/run.sh
+++ support/test/unit/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Starting Solidity Linting..."
-docker container run --entrypoint=npx augur/core-test:latest solium --dir . --reporter=gcc
+docker container run --entrypoint=npx augur/augur-core-test:latest solium --dir . --reporter=gcc
 echo "Done Solidity Linting..."
 
 if [[ "$1" == "console" ]]; then
-  docker container run --rm -it --entrypoint /bin/bash augur/core-test:latest
+  docker container run --rm -it --entrypoint /bin/bash augur/augur-core-test:latest
 else
-  docker container run augur/core-test:latest
+  docker container run augur/augur-core-test:latest
 fi
