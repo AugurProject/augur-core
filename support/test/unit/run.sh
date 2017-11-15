9b7e7cc9e8d1baec04d07e0b7a6d6c3ea641a565
Paul Gebheim <paul@onetaste.us>
2017-11-15 13:27:46 -0800

    Old docker interface

--- support/test/unit/run.sh
+++ support/test/unit/run.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Starting Solidity Linting..."
-docker container run --entrypoint=npx augur/augur-core-test:latest solium --dir . --reporter=gcc
+docker run --entrypoint=npx augur/augur-core-test:latest solium --dir . --reporter=gcc
 echo "Done Solidity Linting..."
 
 if [[ "$1" == "console" ]]; then
-  docker container run --rm -it --entrypoint /bin/bash augur/augur-core-test:latest
+  docker run -rm -it --entrypoint /bin/bash augur/augur-core-test:latest
 else
-  docker container run augur/augur-core-test:latest
+  docker run -rm -it augur/augur-core-test:latest
 fi
