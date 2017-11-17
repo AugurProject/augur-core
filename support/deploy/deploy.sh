603fd9f429f62c7c3fd893846993f0f04f525e13
Paul Gebheim <paul@onetaste.us>
2017-11-16 22:59:34 -0800

    Run deploy/artifacts with env vars

--- support/deploy/deploy.sh
+++ support/deploy/deploy.sh
@@ -1,2 +1,2 @@
-[[ "${DEPLOY}x" == "true" ]] && node output/deployment/deployContracts.js
-[[ "${ARTIFACTS}x" == "true" ]] && npm run artifacts
+[[ "${DEPLOY}" == "true" ]] && node output/deployment/deployContracts.js
+[[ "${ARTIFACTS}" == "true" ]] && npm run artifacts
