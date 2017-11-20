b138519cb9cb82b310e4319db60b8042ee19548f
Paul Gebheim <paul@onetaste.us>
2017-11-20 01:58:39 -0800

    Deploy from tags

--- support/artifacts/run.sh
+++ support/artifacts/run.sh
@@ -7,17 +7,35 @@ if [[ "$TRAVIS" == "true" ]]; then
   fi
 
   branch=$TRAVIS_BRANCH
+  tag=$TRAVIS_TAG
   commit=$TRAVIS_COMMIT
   repo_url=https://$GITHUB_DEPLOYMENT_TOKEN@github.com/AugurProject/augur-contracts
 else
   branch=$(git rev-parse --abbrev-ref HEAD)
   commit=$(git rev-parse --short HEAD)
-  repo_url=https://github.com/AugurProject/augur-contracts
+  tag=$(git name-rev --tags --name-only $(git rev-parse HEAD) | sed "s/\^0//")
+  if [[ "${tag}" != "undefined" ]]; then
+    branch=$tag
+  else
+    tag=''
+  fi
+
+  if [[ "${AUGUR_CONTRACTS_REPO_URL}x" == "x" ]]; then
+    repo_url=https://github.com/AugurProject/augur-contracts
+    echo "AUGUR_CONTRACTS_REPO_URL not set, defaulting to ${repo_url}"
+  else
+    repo_url=$AUGUR_CONTRACTS_REPO_URL
+  fi
 fi
 
 git clone $repo_url output/augur-contracts
-echo $repo_url
+current_dir=$PWD;
 cd output/augur-contracts
+git checkout -b paul origin/paul
 npm install
-BRANCH=$branch COMMIT=$commit SOURCE=../contracts npm run update-contracts
 
+  BRANCH=$branch COMMIT=$commit TAG=$tag SOURCE=../contracts npm run update-contracts
+update_success=$?
+cd $current_dir
+
+exit $update_success
