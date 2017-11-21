30a1c7151b52cafbf4834303cd53e879f375fb4a
Paul Gebheim <paul@onetaste.us>
2017-11-21 15:48:20 -0800

    Die if deploy fails

--- support/deploy/deploy.sh
+++ support/deploy/deploy.sh
@@ -1,2 +1,2 @@
-[[ "${DEPLOY}" == "true" ]] && node output/deployment/deployContracts.js
-[[ "${ARTIFACTS}" == "true" ]] && npm run artifacts
+[[ "${DEPLOY}" == "true" ]] && node output/deployment/deployContracts.js || exit 1
+[[ "${ARTIFACTS}" == "true" ]] && npm run artifacts || exit 1
