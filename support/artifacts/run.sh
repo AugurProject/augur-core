baf720501216a3e9939f3254a9a405183d8e7805
Paul Gebheim <paul@onetaste.us>
2017-11-27 11:37:03 -0800

    Support updating augur.js as well as augur-contracts

--- support/artifacts/run.sh
+++ support/artifacts/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+###
+# Setup the environments properly for merging and updating items in augur-contracts and augur.js
+# a lot of this is because we need to rely on Travis environment because it checks out in a
+# detached head state so we don't good branch information out of our local git repo
+
+
 if [[ "$TRAVIS" == "true" ]]; then
   if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     echo "Skipping updating augur-contracts for pull request ${TRAVIS_PULL_REQUEST}"
@@ -13,7 +19,8 @@ if [[ "$TRAVIS" == "true" ]]; then
   branch=$TRAVIS_BRANCH
   tag=$TRAVIS_TAG
   commit=$TRAVIS_COMMIT
-  repo_url=https://$GITHUB_DEPLOYMENT_TOKEN@github.com/AugurProject/augur-contracts
+  contracts_repo_url=https://$GITHUB_DEPLOYMENT_TOKEN@github.com/AugurProject/augur-contracts
+  augurjs_repo_url=https://$GITHUB_DEPLOYMENT_TOKEN@github.com/AugurProject/augur.js
 else
   branch=$(git rev-parse --abbrev-ref HEAD)
   commit=$(git rev-parse --short HEAD)
@@ -24,12 +31,12 @@ else
     tag=''
   fi
 
-  if [[ "${AUGUR_CONTRACTS_REPO_URL}x" == "x" ]]; then
-    repo_url=https://github.com/AugurProject/augur-contracts
-    echo "AUGUR_CONTRACTS_REPO_URL not set, defaulting to ${repo_url}"
-  else
-    repo_url=$AUGUR_CONTRACTS_REPO_URL
-  fi
+  contracts_repo_url=${AUGUR_CONTRACTS_REPO_URL:-https://github.com/AugurProject/augur-contracts}
+  echo "AUGUR_CONTRACTS_REPO_URL=${contracts_repo_url}, set environment to override"
+
+  augurjs_repo_url=${AUGUR_CONTRACTS_REPO_URL:-https://github.com/AugurProject/augur.js}
+  echo "AUGURJS_REPO_URL=${contracts_repo_url}, set environment to override"
+
 fi
 
 if [[ -e "$HOME/.npmrc" ]]; then
@@ -41,14 +48,43 @@ else
   echo "No logged in NPM session and NPM_TOKEN not set, will not be able to publish to NPM"
 fi
 
-git clone $repo_url output/augur-contracts
-current_dir=$PWD;
+
+###
+# Update the augur-contracts Repository
+
+git clone $contracts_repo_url output/augur-contracts
+current_dir=$PWD
 cd output/augur-contracts
 
 npm install
 
 AUTOCOMMIT=true BRANCH=$branch COMMIT=$commit TAG=$tag SOURCE=../contracts npm run update-contracts
 update_success=$?
+
+contracts_version=$(npm version)
 cd $current_dir
 
-exit $update_success
+[[ $update_success == 0 ]] || exit $update_success
+
+
+###
+# Update the augur.js repository
+
+git clone $augurjs_repo_url output/augur.js
+current_dir=$PWD
+cd output/augur.js
+
+npm install augur-contracts@dev
+npm upgrade augur-contracts
+npm version prerelease
+
+if [[ "$AUTOCOMMIT" == "true" ]]; then
+  git push && git push --tags && npm publish --tag dev
+  update_success=$?
+else
+  update_success=0
+fi
+
+cd $current_dir
+
+exit ${update_success}
