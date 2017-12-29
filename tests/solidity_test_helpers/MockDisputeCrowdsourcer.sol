982f1cc3f0efd21bae7c1cd70bea56e0e5c30bf7
Tom Haile <bradford.haile@gmail.com>
2017-12-29 16:48:16 -0600

    added more unit test coverage for market

--- tests/solidity_test_helpers/MockDisputeCrowdsourcer.sol
+++ tests/solidity_test_helpers/MockDisputeCrowdsourcer.sol
@@ -6,59 +6,89 @@ import 'TEST/MockVariableSupplyToken.sol';
 
 
 contract MockDisputeCrowdsourcer is IDisputeCrowdsourcer, MockVariableSupplyToken {
+    bool private contributeWasCalledValue;
+    uint256 private contributeAmountValue;
+    address private contributeParticipantValue;
+    bytes32 private payoutDistributionHashValue;
+    uint256[] private payoutNumeratorsValue;
+    uint256 private setSizeValue;
+
     function initialize(IMarket market, uint256 _size, bytes32 _payoutDistributionHash, uint256[] _payoutNumerators, bool _invalid) public returns (bool) {
         return true;
     }
 
+    function contributeWasCalled() public returns(bool) { return contributeWasCalledValue; }
+
+    function getContributeParticipant() public returns(address) { return contributeParticipantValue; }
+
+    function getContributeAmount() public returns(uint256) { return contributeAmountValue; }
+
     function contribute(address _participant, uint256 _amount) public returns (uint256) {
+        contributeWasCalledValue = true;
+        contributeParticipantValue = _participant;
+        contributeAmountValue = _amount;
         return 0;
     }
-    
+
     function disavow() public returns (bool) {
         return true;
     }
-    
+
     function getStake() public view returns (uint256) {
         return 0;
     }
-    
-    function getPayoutDistributionHash() public view returns (bytes32) {
-        return bytes32(0);
+
+    function setPayoutNumeratorsValue(uint256[] _payoutNumerators) public {
+        payoutNumeratorsValue = _payoutNumerators;
     }
-    
+
+    function setPayoutDistributionHash(bytes32 _hash) public {
+        payoutDistributionHashValue = _hash;
+    }
+
+    function getPayoutDistributionHash() public view returns (bytes32) {
+        return payoutDistributionHashValue;
+    }
+
     function liquidateLosing() public returns (bool) {
         return true;
     }
-    
+
     function fork() public returns (bool) {
         return true;
     }
-    
+
     function redeem(address _redeemer) public returns (bool) {
         return true;
     }
-    
+
     function isInvalid() public view returns (bool) {
         return true;
     }
-    
+
     function isDisavowed() public view returns (bool) {
         return true;
     }
-    
+
     function migrate() public returns (bool) {
         return true;
     }
-    
+
     function getPayoutNumerator(uint8 _outcome) public view returns (uint256) {
         return 0;
     }
-    
+
     function getMarket() public view returns (IMarket) {
         return IMarket(0);
     }
-    
+
+    function setSize(uint256 _size) public { setSizeValue = _size; }
+
     function getSize() public view returns (uint256) {
-        return 0;
+        return setSizeValue;
+    }
+
+    function callFinishedCrowdsourcingDisputeBond(IMarket _market) public returns(bool) {
+        return _market.finishedCrowdsourcingDisputeBond();
     }
 }
