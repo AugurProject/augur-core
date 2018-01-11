a24ad8d0e4b95780e9c36c3e3a917a3f6428ec41
unknown <achap5dk@gmail.com>
2018-01-11 15:51:52 -0800

    Trading tests and update README with some testing hints

--- tests/trading/test_fillOrder.py
+++ tests/trading/test_fillOrder.py
@@ -138,3 +138,154 @@ def test_publicFillOrder_bid_scalar(contractsFixture, cash, scalarMarket, univer
     assert orders.getBetterOrderId(orderID) == longTo32Bytes(0)
     assert orders.getWorseOrderId(orderID) == longTo32Bytes(0)
     assert fillOrderID == 0
+
+def test_fill_order_with_shares_escrowed_sell_with_shares(contractsFixture, cash, market, universe):
+    createOrder = contractsFixture.contracts['CreateOrder']
+    fillOrder = contractsFixture.contracts['FillOrder']
+    orders = contractsFixture.contracts['Orders']
+    completeSets = contractsFixture.contracts['CompleteSets']
+    yesShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(YES))
+    noShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(NO))
+
+    # buy complete sets for both users
+    assert completeSets.publicBuyCompleteSets(market.address, fix(1), sender=tester.k1, value=fix('1', '10000'))
+    assert yesShareToken.balanceOf(tester.a1) == fix(1)
+    assert completeSets.publicBuyCompleteSets(market.address, fix(1), sender=tester.k2, value=fix('1', '10000'))
+    assert noShareToken.balanceOf(tester.a2) == fix(1)
+
+    # create order with shares
+    logs = []
+    captureFilteredLogs(contractsFixture.chain.head_state, contractsFixture.contracts['Augur'], logs)
+    orderID = createOrder.publicCreateOrder(ASK, fix(1), 6000, market.address, YES, longTo32Bytes(0), longTo32Bytes(0), "42", sender=tester.k1)
+    assert orderID
+
+    # fill order with shares
+    assert fillOrder.publicFillOrder(orderID, fix(1), "43", sender=tester.k2) == 0
+
+    assert len(logs) == 8
+    log1 = logs[7]
+
+    assert log1["_event_type"] == "OrderFilled"
+    assert log1["filler"] == bytesToHexString(tester.a2)
+    assert log1["numCreatorShares"] == fix(1)
+    assert log1["numCreatorTokens"] == 0
+    assert log1["numFillerShares"] == fix(1)
+    assert log1["numFillerTokens"] == 0
+    assert log1["marketCreatorFees"] == fix(1, 10000) / market.getMarketCreatorSettlementFeeDivisor()
+    assert log1["reporterFees"] == fix(1, 10000) / universe.getOrCacheReportingFeeDivisor()
+    assert log1["shareToken"] == market.getShareToken(YES)
+    assert log1["tradeGroupId"] == stringToBytes("43")
+
+    assert orders.getAmount(orderID) == 0
+    assert orders.getPrice(orderID) == 0
+    assert orders.getOrderCreator(orderID) == longToHexString(0)
+    assert orders.getOrderMoneyEscrowed(orderID) == 0
+    assert orders.getOrderSharesEscrowed(orderID) == 0
+    assert orders.getBetterOrderId(orderID) == longTo32Bytes(0)
+    assert orders.getWorseOrderId(orderID) == longTo32Bytes(0)
+
+def test_fill_order_with_shares_escrowed_sell_with_shares_categorical(contractsFixture, cash, categoricalMarket, universe):
+    market = categoricalMarket
+    createOrder = contractsFixture.contracts['CreateOrder']
+    fillOrder = contractsFixture.contracts['FillOrder']
+    orders = contractsFixture.contracts['Orders']
+    completeSets = contractsFixture.contracts['CompleteSets']
+    firstShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(0))
+    secondShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(1))
+    thirdShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(2))
+
+    # buy complete sets for both users
+    numTicks = market.getNumTicks()
+    assert completeSets.publicBuyCompleteSets(market.address, fix(1), sender=tester.k1, value=fix('1', numTicks))
+    assert completeSets.publicBuyCompleteSets(market.address, fix(1), sender=tester.k2, value=fix('1', numTicks))
+    assert firstShareToken.balanceOf(tester.a1) == firstShareToken.balanceOf(tester.a2) == fix(1)
+    assert secondShareToken.balanceOf(tester.a1) == secondShareToken.balanceOf(tester.a2) == fix(1)
+    assert thirdShareToken.balanceOf(tester.a1) == thirdShareToken.balanceOf(tester.a2) == fix(1)
+
+    # create order with shares
+    logs = []
+    price = 6000
+    captureFilteredLogs(contractsFixture.chain.head_state, contractsFixture.contracts['Augur'], logs)
+    orderID = createOrder.publicCreateOrder(ASK, fix(1), price, market.address, 0, longTo32Bytes(0), longTo32Bytes(0), "42", sender=tester.k1)
+    assert orderID
+
+    # fill order with shares
+    assert fillOrder.publicFillOrder(orderID, fix(1), "43", sender=tester.k2) == 0
+
+    # The second users corresponding shares were used to fulfil this order
+    assert firstShareToken.balanceOf(tester.a2) == fix(1)
+    assert secondShareToken.balanceOf(tester.a2) == 0
+    assert thirdShareToken.balanceOf(tester.a2) == 0
+
+    assert len(logs) == 10
+    log1 = logs[9]
+
+    assert log1["_event_type"] == "OrderFilled"
+    assert log1["filler"] == bytesToHexString(tester.a2)
+    assert log1["numCreatorShares"] == fix(1)
+    assert log1["numCreatorTokens"] == 0
+    assert log1["numFillerShares"] == fix(1)
+    assert log1["numFillerTokens"] == 0
+    assert log1["marketCreatorFees"] == fix(1, numTicks) / market.getMarketCreatorSettlementFeeDivisor()
+    assert log1["reporterFees"] == fix(1, numTicks) / universe.getOrCacheReportingFeeDivisor()
+    assert log1["shareToken"] == market.getShareToken(0)
+    assert log1["tradeGroupId"] == stringToBytes("43")
+
+    assert orders.getAmount(orderID) == 0
+    assert orders.getPrice(orderID) == 0
+    assert orders.getOrderCreator(orderID) == longToHexString(0)
+    assert orders.getOrderMoneyEscrowed(orderID) == 0
+    assert orders.getOrderSharesEscrowed(orderID) == 0
+    assert orders.getBetterOrderId(orderID) == longTo32Bytes(0)
+    assert orders.getWorseOrderId(orderID) == longTo32Bytes(0)
+
+def test_fill_buy_order_with_buy_categorical(contractsFixture, cash, categoricalMarket, universe):
+    market = categoricalMarket
+    createOrder = contractsFixture.contracts['CreateOrder']
+    fillOrder = contractsFixture.contracts['FillOrder']
+    orders = contractsFixture.contracts['Orders']
+    firstShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(0))
+    secondShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(1))
+    thirdShareToken = contractsFixture.applySignature('ShareToken', market.getShareToken(2))
+
+    # create order with cash
+    logs = []
+    price = 6000
+    numTicks = market.getNumTicks()
+    captureFilteredLogs(contractsFixture.chain.head_state, contractsFixture.contracts['Augur'], logs)
+    orderID = createOrder.publicCreateOrder(BID, fix(1), price, market.address, 0, longTo32Bytes(0), longTo32Bytes(0), "42", value=fix(1, price), sender=tester.k1)
+    assert orderID
+
+    # fill order with cash
+    assert fillOrder.publicFillOrder(orderID, fix(1), "43", sender=tester.k2, value=fix(1, numTicks - price)) == 0
+
+    # A complete set was purchased with the provided cash and the shares were provided to each user
+    assert firstShareToken.balanceOf(tester.a1) == fix(1)
+    assert secondShareToken.balanceOf(tester.a1) == 0
+    assert thirdShareToken.balanceOf(tester.a1) == 0
+    
+    assert firstShareToken.balanceOf(tester.a2) == 0
+    assert secondShareToken.balanceOf(tester.a2) == fix(1)
+    assert thirdShareToken.balanceOf(tester.a2) == fix(1)
+
+    assert len(logs) == 8
+    log1 = logs[7]
+
+    assert log1["_event_type"] == "OrderFilled"
+    assert log1["filler"] == bytesToHexString(tester.a2)
+    assert log1["numCreatorShares"] == 0
+    assert log1["numCreatorTokens"] == fix(1, price)
+    assert log1["numFillerShares"] == 0
+    assert log1["numFillerTokens"] == fix(1, numTicks - price)
+    assert log1["marketCreatorFees"] == 0
+    assert log1["reporterFees"] == 0
+    assert log1["shareToken"] == market.getShareToken(0)
+    assert log1["tradeGroupId"] == stringToBytes("43")
+
+    assert orders.getAmount(orderID) == 0
+    assert orders.getPrice(orderID) == 0
+    assert orders.getOrderCreator(orderID) == longToHexString(0)
+    assert orders.getOrderMoneyEscrowed(orderID) == 0
+    assert orders.getOrderSharesEscrowed(orderID) == 0
+    assert orders.getBetterOrderId(orderID) == longTo32Bytes(0)
+    assert orders.getWorseOrderId(orderID) == longTo32Bytes(0)
