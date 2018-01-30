f5b9e5b29f7d5f3537590bfd5cd2378f774bf518
Paul Gebheim <paul@onetaste.us>
2018-01-30 13:47:58 -0800

    Fancy export syntax and browser module

--- source/index.ts
+++ source/index.ts
@@ -1,8 +1,5 @@
-import { NetworkConfiguration as _NetworkConfiguration } from "./libraries/NetworkConfiguration";
-import { DeployerConfiguration as _DeployerConfiguration } from "./libraries/DeployerConfiguration";
-import { ContractDeployer as _ContractDeployer } from "./libraries/ContractDeployer";
+export { NetworkConfiguration } from "./libraries/NetworkConfiguration";
+export { DeployerConfiguration } from "./libraries/DeployerConfiguration";
+export { ContractDeployer } from "./libraries/ContractDeployer";
 
 export const abi = require('./contracts/abi');
-export const NetworkConfigurationq = _NetworkConfiguration;
-export const DeployerConfiguration = _DeployerConfiguration;
-export const ContractDeployer = _ContractDeployer;
