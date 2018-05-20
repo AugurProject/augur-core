b1a9a4ee8c47dfd7f6faf8c27c68136155b5de03
Jack Peterson <jack@tinybike.net>
2018-05-19 21:26:29 -0700

    Separate MINIMUM_GAS_NEEDED constants for order creation and order filling; only call orders.setPrice if actually filling

--- source/contracts/trading/Trade.sol
+++ source/contracts/trading/Trade.sol
@@ -15,7 +15,8 @@ import 'libraries/CashAutoConverter.sol';
 
 
 contract Trade is CashAutoConverter, ReentrancyGuard, MarketValidator {
-    uint256 internal constant MINIMUM_GAS_NEEDED = 500000;
+    uint256 internal constant FILL_ORDER_MINIMUM_GAS_NEEDED = 2000000;
+    uint256 internal constant CREATE_ORDER_MINIMUM_GAS_NEEDED = 3000000;
 
     function publicBuy(IMarket _market, uint256 _outcome, uint256 _fxpAmount, uint256 _price, bytes32 _betterOrderId, bytes32 _worseOrderId, bytes32 _tradeGroupId) external payable marketIsLegit(_market) convertToAndFromCash onlyInGoodTimes returns (bytes32) {
         bytes32 _result = trade(msg.sender, Order.TradeDirections.Long, _market, _outcome, _fxpAmount, _price, _betterOrderId, _worseOrderId, _tradeGroupId);
@@ -47,7 +48,7 @@ contract Trade is CashAutoConverter, ReentrancyGuard, MarketValidator {
         if (_bestFxpAmount == 0) {
             return bytes32(1);
         }
-        if (msg.gas < getMinGasNeeded()) {
+        if (msg.gas < getCreateOrderMinGasNeeded()) {
             return bytes32(1);
         }
         Order.Types _type = Order.getOrderTradingTypeFromMakerDirection(_direction);
@@ -60,13 +61,13 @@ contract Trade is CashAutoConverter, ReentrancyGuard, MarketValidator {
         IOrders _orders = IOrders(controller.lookup("Orders"));
         bytes32 _orderId = _orders.getBestOrderId(_type, _market, _outcome);
         _bestFxpAmount = _fxpAmount;
-        while (_orderId != 0 && _bestFxpAmount > 0 && msg.gas >= getMinGasNeeded()) {
+        while (_orderId != 0 && _bestFxpAmount > 0 && msg.gas >= getFillOrderMinGasNeeded()) {
             uint256 _orderPrice = _orders.getPrice(_orderId);
             // If the price is acceptable relative to the trade type
             if (_type == Order.Types.Bid ? _orderPrice >= _price : _orderPrice <= _price) {
-                _orders.setPrice(_market, _outcome, _orderPrice);
                 bytes32 _nextOrderId = _orders.getWorseOrderId(_orderId);
                 if (_orders.getOrderCreator(_orderId) != _sender) {
+                    _orders.setPrice(_market, _outcome, _orderPrice);
                     _bestFxpAmount = IFillOrder(controller.lookup("FillOrder")).fillOrder(_sender, _orderId, _bestFxpAmount, _tradeGroupId);
                 }
                 _orderId = _nextOrderId;
@@ -78,7 +79,10 @@ contract Trade is CashAutoConverter, ReentrancyGuard, MarketValidator {
     }
 
     // COVERAGE: This is not covered and cannot be. We need to use a different minimum gas while running coverage since the additional logging make the cost rise a great deal
-    function getMinGasNeeded() internal pure returns (uint256) {
-        return MINIMUM_GAS_NEEDED;
+    function getFillOrderMinGasNeeded() internal pure returns (uint256) {
+        return FILL_ORDER_MINIMUM_GAS_NEEDED;
+    }
+    function getCreateOrderMinGasNeeded() internal pure returns (uint256) {
+        return CREATE_ORDER_MINIMUM_GAS_NEEDED;
     }
 }
