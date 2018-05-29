8a24b55ec83aa052d56c4c9094ef524fb8268d6d
nuevoalex <achap5dk@gmail.com>
2018-05-28 17:06:22 -0700

    undo debug testing thing

--- source/contracts/legacy_reputation/StandardToken.sol
+++ source/contracts/legacy_reputation/StandardToken.sol
@@ -22,15 +22,15 @@ contract StandardToken is ERC20, BasicToken {
      * @param _value uint256 the amout of tokens to be transfered
      */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
-        //uint256 _allowance = allowed[_from][msg.sender];
+        uint256 _allowance = allowed[_from][msg.sender];
 
         // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-        // require (_value <= _allowance);
+        require (_value <= _allowance);
 
-        //balances[_to] = balances[_to].add(_value);
-        //balances[_from] = balances[_from].sub(_value);
-        //allowed[_from][msg.sender] = _allowance.sub(_value);
-        //Transfer(_from, _to, _value);
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
         return true;
     }
 
