9a68b993f4e3c3f4264f2d9ec5f8a1d8c91e448f
unknown <achap5dk@gmail.com>
2018-08-22 15:43:51 -0700

    Fix for forking issue

--- tests/reporting/test_stake_calculations.py
+++ tests/reporting/test_stake_calculations.py
@@ -1,6 +1,6 @@
 from ethereum.tools import tester
 from ethereum.tools.tester import TransactionFailed
-from utils import captureFilteredLogs, bytesToHexString, AssertLog
+from utils import captureFilteredLogs, bytesToHexString, AssertLog, TokenDelta, longToHexString
 from pytest import raises
 from reporting_utils import proceedToNextRound
 
@@ -21,3 +21,29 @@ def test_initial_report_bond_size(contractsFixture, market, universe):
     designatedReportCost = universe.getOrCacheDesignatedReportStake()
     initialReporter = contractsFixture.applySignature('InitialReporter', initialReporterAddress)
     assert initialReporter.getSize() == designatedReportCost
+
+def test_fork_bonus(contractsFixture, market, universe):
+    # proceed to forking
+    while (market.getForkingMarket() == longToHexString(0)):
+        proceedToNextRound(contractsFixture, market)
+
+    reputationToken = contractsFixture.applySignature('ReputationToken', universe.getReputationToken())
+    payoutHash = market.derivePayoutDistributionHash([market.getNumTicks(), 0], False)
+    childUniverse = contractsFixture.applySignature("Universe", universe.createChildUniverse([market.getNumTicks(), 0], False))
+    childReputationToken = contractsFixture.applySignature("ReputationToken", childUniverse.getReputationToken())
+
+    # We'll transfer some additional REP to the initial reporter contract to try and get a larger bonus
+    initialReporterAddress = market.getInitialReporter()
+    extraAmount = 10000
+    assert reputationToken.transfer(initialReporterAddress, extraAmount)
+
+    # When we redeem the bonus we will only be given a bonus relative to the legitimate stake in the bond
+    designatedReportCost = universe.getOrCacheDesignatedReportStake()
+    initialReporter = contractsFixture.applySignature('InitialReporter', initialReporterAddress)
+
+    # Skip to after the fork end to not deal with 5% bonus math
+    contractsFixture.contracts["Time"].setTimestamp(universe.getForkEndTime() + 1)
+
+    expectedPayout = designatedReportCost + (designatedReportCost / 2) + extraAmount
+    with TokenDelta(childReputationToken, expectedPayout, tester.a0, "Redeeming forked bond didn't result in the expected amount"):
+        assert initialReporter.forkAndRedeem()
